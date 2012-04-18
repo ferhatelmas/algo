923cb78f76854910df7dd1df1ef6e7513f176170
ferhat elmas <elmas.ferhat@gmail.com>
2012-04-18 10:33:54 +0200

    checkpoint

--- euler/src/com/ferhatelmas/euler/page4/Question187.java
+++
@@ -1,9 +0,0 @@
-package com.ferhatelmas.euler.page4;
-
-/**
- * User: felmas
- * Date: 4/18/12
- * Time: 2:01 AM
- */
-public class Question187 {
-}
--- euler/src/com/ferhatelmas/euler/page8/Question377.java
+++
@@ -1,9 +0,0 @@
-package com.ferhatelmas.euler.page8;
-
-/**
- * User: felmas
- * Date: 4/16/12
- * Time: 8:59 PM
- */
-public class Question377 {
-}
