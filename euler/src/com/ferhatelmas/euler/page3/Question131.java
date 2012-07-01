6e9bed569532c9e9de1a4605f613b31851de9695
ferhat elmas <elmas.ferhat@gmail.com>
2012-07-01 16:49:55 +0200

    added euler 131 and 146

--- euler/src/com/ferhatelmas/euler/page3/Question131.java
+++ euler/src/com/ferhatelmas/euler/page3/Question131.java
@@ -1,9 +1,32 @@
 package com.ferhatelmas.euler.page3;
 
-/**
- * User: felmas
- * Date: 7/1/12
- * Time: 1:29 AM
- */
+import java.util.Arrays;
+
 public class Question131 {
+
+  public static void main(String[] args) {
+
+    boolean[] sieve = new boolean[1000001];
+    Arrays.fill(sieve, true);
+
+    for(int i=2; i<sieve.length; i++) {
+      if(sieve[i]) {
+        for(int j=2*i; j<sieve.length; j+=i) {
+          sieve[j] = false;
+        }
+      }
+    }
+
+    long i = 1, j = 2;
+    int cnt = 0, diff = (int)(j*j*j - i*i*i);
+    while(diff < 1000000) {
+      if(sieve[diff]) cnt++;
+      i++;
+      j++;
+      diff = (int)(j*j*j - i*i*i);
+    }
+
+    System.out.println(cnt);
+  }
+
 }
--- euler/src/com/ferhatelmas/euler/page3/Question146.java
+++ euler/src/com/ferhatelmas/euler/page3/Question146.java
@@ -1,9 +1,31 @@
 package com.ferhatelmas.euler.page3;
 
-/**
- * User: felmas
- * Date: 7/1/12
- * Time: 1:55 AM
- */
+import java.math.BigInteger;
+import java.util.HashSet;
+
 public class Question146 {
+
+  public static void main(String[] args) {
+
+    HashSet<Integer> index = new HashSet<Integer>();
+    index.add(2);index.add(6);index.add(8);index.add(12);index.add(26);
+
+    long sum = 0;
+    for(int i=10; i<150000000; i+=10) {
+
+      if(i%3 == 0 || i%7 == 0 || i%13 == 0) continue;
+
+      long val = (long)i*i+1;
+      if(!new BigInteger(String.valueOf(val)).isProbablePrime(10)) continue;
+
+      for(int j=1; j<27; j++) {
+        boolean isPrime = new BigInteger(String.valueOf(val+j)).isProbablePrime(10);
+        if((isPrime && !index.contains(j)) || (!isPrime && index.contains(j))) break;
+        if(isPrime && j == 26) sum += i;
+      }
+
+    }
+
+    System.out.println(sum);
+  }
 }
