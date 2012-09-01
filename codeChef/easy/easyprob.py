523d2905e860b49c628fba5e3f05de24b41e71ee
ferhat elmas <elmas.ferhat@gmail.com>
2012-09-01 18:14:48 +0200

    added chef easyprob

---
+++ codeChef/easy/easyprob.py
@@ -0,0 +1,20 @@
+from math import log 
+
+def f(n):
+  p = int(log(n, 2))
+  c = n-2**p
+
+  s = "2(" if p != 1 else "2"
+  if p >= 2:
+    s += f(p)
+  elif p == 0:
+    s += str(p)
+  s += ")" if p != 1 else ""
+
+  if c > 0:
+    s += "+" + f(c) 
+  return s 
+
+numbers = [137, 1315, 73, 136, 255, 1384, 16385]
+for n in numbers:
+  print "%d=%s" % (n, f(n))
