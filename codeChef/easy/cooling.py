0efb4358faec895d1c22ee0dc926ebd96e6fb591
ferhat elmas <elmas.ferhat@gmail.com>
2012-09-03 04:50:09 +0200

    added chef cooling

---
+++ codeChef/easy/cooling.py
@@ -0,0 +1,12 @@
+for _ in xrange(int(raw_input())):
+  l = int(raw_input())
+  pies = sorted(map(int, raw_input().split()))
+  racks = sorted(map(int, raw_input().split()))
+
+  i, j = 0, 0
+  while i < l and j < l:
+    if pies[i] <= racks[j]:
+      i +=1 
+    j += 1
+
+  print i
