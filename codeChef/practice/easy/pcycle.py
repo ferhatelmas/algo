3cb06011ed1a2d009e640d3ed32d4bb719bb82e1
ferhat elmas <elmas.ferhat@gmail.com>
2012-09-12 23:18:45 +0200

    added chef pcycle

---
+++ codeChef/practice/easy/pcycle.py
@@ -0,0 +1,17 @@
+n = int(raw_input())
+p = [0] + map(int, raw_input().split())
+m, c, s, cs = range(1, n+1), [], 1, []
+while(len(m) >= 0):
+  if s in c:
+    cs.append(" ".join(map(str, c) + [str(s)]))
+    c = []
+    if len(m) == 0:
+      break
+    s = m[0]
+    
+  c.append(s)
+  m.remove(s)
+  s = p[s]
+print len(cs)
+for c in cs:
+  print c
