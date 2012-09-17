10e5d6bfa72171a0a9520050ff73b32eae1332d5
ferhat elmas <elmas.ferhat@gmail.com>
2012-09-17 09:09:04 +0200

    added chef decstr

---
+++ codeChef/practice/easy/decstr.py
@@ -0,0 +1,7 @@
+for _ in xrange(int(raw_input())):
+  n = int(raw_input())
+  r = map(chr, (range(97, 123) * (n/25)))
+  if n%25 > 0:
+    r += map(chr, range(97, 98+n%25))
+  r.reverse()
+  print "".join(r)
