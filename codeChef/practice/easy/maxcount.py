df7546ad307f1871d126434e7173dfca5152ad03
ferhat elmas <elmas.ferhat@gmail.com>
2012-09-29 22:33:48 +0200

    added chef maxcount

---
+++ codeChef/practice/easy/maxcount.py
@@ -0,0 +1,15 @@
+for _ in xrange(int(raw_input())):
+  raw_input()
+  d = {}
+  mc, mi = 0, None
+  for e in raw_input().split():
+    if e in d:
+      d[e] += 1
+    else:
+      d[e] = 1
+
+    if d[e] > mc or (d[e] == mc and int(e) < int(mi)):
+      mc = d[e]
+      mi = e
+
+  print "%s %d" % (mi, mc)
