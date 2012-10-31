bcf09b0b3c659ee369c18a11c9030392e10cfa30
ferhat elmas <elmas.ferhat@gmail.com>
2012-10-31 01:36:24 +0100

    added eolimp 571

---
+++ eolimp/src/com/ferhatelmas/eolimp/page19/q571/Main.java
@@ -0,0 +1,38 @@
+package com.ferhatelmas.eolimp.page19.q571;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    int n = in.nextInt();
+    int[] numbers = new int[n];
+    for(int i=0; i<n; i++) numbers[i] = in.nextInt();
+    Arrays.sort(numbers);
+
+    n = numbers[0];
+    for(int i=1; i<numbers.length; i++)
+      n = gcd(n, numbers[i]);
+
+    System.out.println(n);
+  }
+
+  private static int gcd(int a, int b) {
+    int t;
+    while(b != 0) {
+      t = a;
+      a = b;
+      b = t%b;
+    }
+    return a;
+  }
+
+  private static int gcd2(int a, int b) {
+    return b == 0 ? a : gcd2(b, a%b);
+  }
+
+}
