c6440ce27382165c2ed4a1a264dd81305d002954
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-01 04:43:49 +0100

    added eolimp 1148

---
+++ eolimp/src/com/ferhatelmas/eolimp/page38/q1148/Main.java
@@ -0,0 +1,19 @@
+package com.ferhatelmas.eolimp.page38.q1148;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int t = in.nextInt();
+
+    while(t-- > 0) {
+      int g = in.nextInt(), l = in.nextInt();
+      System.out.println(l%g == 0 ? (g + " " + l) : -1);
+    }
+
+  }
+
+}
