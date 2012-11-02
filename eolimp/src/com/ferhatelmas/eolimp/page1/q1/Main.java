47413c4a1e04c9785caa41f1440a4c6808e5c788
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-02 23:32:46 +0100

    added 1

---
+++ eolimp/src/com/ferhatelmas/eolimp/page1/q1/Main.java
@@ -0,0 +1,12 @@
+package com.ferhatelmas.eolimp.page1.q1;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception{
+    int r = new Scanner(System.in).nextInt();
+    System.out.println(r/10 + " " + r%10);
+  }
+
+}
