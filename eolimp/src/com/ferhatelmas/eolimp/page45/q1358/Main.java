14f017151528d284210e8d89b20dcf8f11585df3
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-05 00:40:51 +0100

    added eolimp 1358

---
+++ eolimp/src/com/ferhatelmas/eolimp/page45/q1358/Main.java
@@ -0,0 +1,34 @@
+package com.ferhatelmas.eolimp.page45.q1358;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    long[] ns = new long[n];
+    long max = Long.MIN_VALUE;
+
+    for(int i=0; i<n; i++) {
+      ns[i] = in.nextLong();
+      max = Math.max(max, ns[i]);
+    }
+    Arrays.sort(ns);
+
+    HashSet<Long> fibs = new HashSet<Long>();
+    long l, l1 = 0, l2 = 1;
+    while((l = l1 + l2) <= max) {
+      l1 = l2;
+      l2 = l;
+      fibs.add(l);
+    }
+    int cnt = 0;
+    for(long v : ns) if(fibs.contains(v)) cnt++;
+    System.out.println(cnt);
+  }
+
+}
