311b67ae36ce5838746a62b08d25f72b96bae86f
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-04 02:24:46 +0100

    added eolimp 2

---
+++ eolimp/src/com/ferhatelmas/eolimp/page1/q2/Main.java
@@ -0,0 +1,11 @@
+package com.ferhatelmas.eolimp.page1.q2;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception{
+    System.out.println(new Scanner(System.in).next().length());
+  }
+
+}
