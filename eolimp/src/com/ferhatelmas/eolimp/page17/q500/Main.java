da84a3b0758336b67e9c55280a89f558359efdde
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-04 15:23:53 +0100

    added eolimp 500

---
+++ eolimp/src/com/ferhatelmas/eolimp/page17/q500/Main.java
@@ -0,0 +1,22 @@
+package com.ferhatelmas.eolimp.page17.q500;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    int n = in.nextInt();
+    int l,w,h;
+
+    for(int i=0; i<n; i++) {
+      l=in.nextInt();
+      w=in.nextInt();
+      h=in.nextInt();
+      System.out.println((int)Math.ceil(((l+w)*h)/8.0));
+    }
+  }
+
+}
