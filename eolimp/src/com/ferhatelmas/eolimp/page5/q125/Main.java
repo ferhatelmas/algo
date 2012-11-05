059cb56eabb63fa3400422cffc7e5cc5ba9dc869
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-05 02:36:59 +0100

    added eolimp 125

---
+++ eolimp/src/com/ferhatelmas/eolimp/page5/q125/Main.java
@@ -0,0 +1,37 @@
+package com.ferhatelmas.eolimp.page5.q125;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+  public static void main(String[] args) {
+
+    Scanner in=new Scanner(System.in);
+
+    int h1=in.nextInt();
+    int m1=in.nextInt();
+    int s1=in.nextInt();
+
+    int h2=in.nextInt();
+    int m2=in.nextInt();
+    int s2=in.nextInt();
+
+    h2-=h1;
+    m2-=m1;
+    s2-=s1;
+
+    if(s2<0) {
+      s2+=60;
+      m2--;
+    }
+
+    if(m2<0) {
+      m2+=60;
+      h2--;
+    }
+
+    System.out.println(h2 + " " + m2 + " " + s2);
+  }
+
+}
