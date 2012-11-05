47e973a89d821c6ffd1d9637bd26b8c029fbdf3f
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-05 19:15:04 +0100

    added eolimp 248

---
+++ eolimp/src/com/ferhatelmas/eolimp/page9/q248/Main.java
@@ -0,0 +1,13 @@
+package com.ferhatelmas.eolimp.page9.q248;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n=in.nextInt();
+    System.out.println(n*(n+1)+1);
+  }
+
+}
