220868b824fccc395751e514e3b4c52faccb0313
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-05 20:23:38 +0100

    added eolimp 929

---
+++ eolimp/src/com/ferhatelmas/eolimp/page31/q929/Main.java
@@ -0,0 +1,24 @@
+package com.ferhatelmas.eolimp.page31.q929;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    in.useLocale(Locale.US);
+
+    double a=in.nextDouble();
+    double b=in.nextDouble();
+    double c=in.nextDouble();
+    double d=in.nextDouble();
+
+    if(a==b && c==d) System.out.println("YES");
+    else if(a==c && b==d) System.out.println("YES");
+    else if(a==d && b==c) System.out.println("YES");
+    else System.out.println("NO");
+  }
+
+}
