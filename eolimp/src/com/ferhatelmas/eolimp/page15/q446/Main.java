b0467f4f78fd82390c45ae835bb7b2076dc91e65
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-05 20:00:52 +0100

    added eolimp 446

---
+++ eolimp/src/com/ferhatelmas/eolimp/page15/q446/Main.java
@@ -0,0 +1,21 @@
+package com.ferhatelmas.eolimp.page15.q446;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int n=new Scanner(System.in).nextInt();
+    int q, d, i, cnt=0;
+
+    for(i=1; i<=n; i++) {
+      q = n / i;
+      d = n - i * q;
+      if(q == d) cnt++;
+    }
+
+    System.out.println(cnt);
+  }
+
+}
