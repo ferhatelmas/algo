21ab8026946deecbe09d0e6a72e1486489d955b2
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-05 01:54:32 +0100

    added easy ones from page1

---
+++ eolimp/src/com/ferhatelmas/eolimp/page1/q20/Main.java
@@ -0,0 +1,24 @@
+package com.ferhatelmas.eolimp.page1.q20;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int n = new Scanner(System.in).nextInt();
+
+    int cnt=0, i;
+    String s;
+
+    while(n>0) {
+      s = String.valueOf(n);
+      for(i=0; i<s.length(); i++)
+        n -= (s.charAt(i) - '0');
+      cnt++;
+    }
+
+    System.out.println(cnt);
+  }
+
+}
---
+++ eolimp/src/com/ferhatelmas/eolimp/page1/q22/Main.java
@@ -0,0 +1,27 @@
+package com.ferhatelmas.eolimp.page1.q22;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner in=new Scanner(System.in);
+    int a = in.nextInt(), b = in.nextInt(), cnt = 0;
+
+    for(int i=Math.min(a, b); i<=Math.max(a, b); i++) {
+      if(isPrime(i) && isPrime(Integer
+          .parseInt(new StringBuilder(String.valueOf(i)).reverse().toString())))
+        cnt++;
+    }
+
+    System.out.println(cnt);
+  }
+
+  private static boolean isPrime(int a) {
+    if(a < 2) return false;
+    for(int i=2; i<=Math.sqrt(a); i++) if(a%i==0) return false;
+    return true;
+  }
+
+}
