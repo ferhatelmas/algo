5abaef2bf6c2a39ed5a3bf572a5ea57dc13d9ef2
ferhat elmas <elmas.ferhat@gmail.com>
2012-11-06 02:42:34 +0100

    added eolimp 1357

---
+++ eolimp/src/com/ferhatelmas/eolimp/page45/q1357/Main.java
@@ -0,0 +1,12 @@
+package com.ferhatelmas.eolimp.page45.q1357;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    System.out.println(Math.min(in.nextInt(), in.nextInt()));
+  }
+
+}
