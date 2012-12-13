805c02e06b5d93c96a9c75316d6e03cebfdd0f82
ferhat elmas <elmas.ferhat@gmail.com>
2012-12-13 23:14:41 +0100

    return fix

--- codeval/medium/string_rotation.py
+++ codeval/medium/string_rotation.py
@@ -2,11 +2,8 @@ import sys
 
 def rotation(ls):
   s = ls[0] + ls[0]
-  if s.find(ls[1]) == -1:
-    return False
-  else:
-    return True
-
+  return s.find(ls[1]) == -1:
+  
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
   print rotation(test.strip().split(","))
