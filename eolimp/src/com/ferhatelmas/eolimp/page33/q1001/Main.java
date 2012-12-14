b9e0eedb3e26be127e54fe12938f490255a3b76a
ferhat elmas <elmas.ferhat@gmail.com>
2012-12-14 17:13:48 +0100

    added eolimp 1001

---
+++ eolimp/src/com/ferhatelmas/eolimp/page33/q1001/Main.java
@@ -0,0 +1,14 @@
+package com.ferhatelmas.eolimp.page33.q1001;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    BigInteger a = new BigInteger(in.next().trim(), 2);
+    System.out.println(a.add(new BigInteger(in.next().trim(), 2)).toString(2));
+  }
+}
