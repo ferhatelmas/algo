e7249eb74d2c6888f821ca5fd7679b99b7becd72
ferhat elmas <elmas.ferhat@gmail.com>
2013-01-18 05:25:48 +0100

    added codeeval armstrong

---
+++ codeEval/easy/armstrong_numbers.py
@@ -0,0 +1,10 @@
+import sys
+
+def arm(n):
+  l = len(n)
+  return sum(map(lambda e: int(e)**l, n)) == int(n)
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+  print arm(test.strip())
+test_cases.close()
