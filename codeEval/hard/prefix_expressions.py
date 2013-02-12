d96d891027fb4f2442beb78ea0823aba387ee556
ferhat elmas <elmas.ferhat@gmail.com>
2013-02-12 13:47:03 +0200

    added codeval prefix expressions

---
+++ codeEval/hard/prefix_expressions.py
@@ -0,0 +1,22 @@
+import sys
+
+def my_eval(ls):
+  ops, args = [], []
+  for e in reversed(ls):
+    (args, ops)[e in '*/+'].append(e)
+  return do_my_eval(ops, map(int, args))
+
+def do_my_eval(ops, args):
+  for op in ops:
+    if op == '+':
+      args.append(args.pop() + args.pop())
+    elif op == '/':
+      args.append(args.pop() / args.pop())
+    else:
+      args.append(args.pop() * args.pop())
+  return args[0]
+    
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+  print my_eval(test.strip().split())
+test_cases.close()
