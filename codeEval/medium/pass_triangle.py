6fa4a801d0b4f138bb742157af716957fe919947
ferhat elmas <elmas.ferhat@gmail.com>
2013-03-20 14:10:01 +0100

    added codeval pass triangle

---
+++ codeEval/medium/pass_triangle.py
@@ -0,0 +1,12 @@
+import sys
+
+def update_row(res, ls):
+  return [max(x, y) + z for (x, y, z) in zip([0] + res, res + [0], ls)]
+
+res = []
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+  res = update_row(res, map(int, test.strip().split()))
+test_cases.close()
+
+print max(res)
