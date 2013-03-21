25326d86b9e66e33b58abe600cb717fe11a9c01b
ferhat elmas <elmas.ferhat@gmail.com>
2013-03-21 23:16:37 +0100

    added eolimp 313

---
+++ eolimp/src/com/ferhatelmas/eolimp/page11/q313/Main.java
@@ -0,0 +1,18 @@
+package com.ferhatelmas.eolimp.page11.q313;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+
+    in.nextLine();
+    while(n-- > 0) {
+      String s = in.nextLine();
+      int k = s.indexOf('+');
+      System.out.println(new BigInteger(s.substring(0, k)).add(new BigInteger(s.substring(k+1))));
+    }
+  }
+}
