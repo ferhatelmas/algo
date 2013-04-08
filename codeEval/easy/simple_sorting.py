81786920375fa56a8b3e8e70b4f078426ad87455
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-08 18:05:37 +0200

    added codeval simple sorting

---
+++ codeEval/easy/simple_sorting.py
@@ -0,0 +1,6 @@
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+  print " ".join(map(lambda f: "%.3f" % (f,), sorted(map(float, test.strip().split()))))
+test_cases.close()
