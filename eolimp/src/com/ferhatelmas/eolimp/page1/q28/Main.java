5cd6589c3e55437b0cc042a71e2958b7a4b5a71d
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-22 21:51:24 +0200

    added eolimp 28

---
+++ eolimp/src/com/ferhatelmas/eolimp/page1/q28/Main.java
@@ -0,0 +1,24 @@
+package com.ferhatelmas.eolimp.page1.q28;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    BigInteger r = new BigInteger(new Scanner(System.in).nextLine()), curr;
+
+    for(int i=2; i<=10000; i++) {
+      curr = BigInteger.ONE;
+      for(int j=i; j<=10000; j++) {
+        curr = curr.multiply(new BigInteger(String.valueOf(j)));
+        int cmp = curr.compareTo(r);
+        if(cmp == 0) {
+          System.out.println(i + " " + j);
+          return;
+        } else if(cmp > 0)
+          break;
+      }
+    }
+  }
+}
