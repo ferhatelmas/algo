2e456e8caf21b1756e1c869742521713ad09cd89
ferhat elmas <elmas.ferhat@gmail.com>
2013-05-10 00:15:16 +0200

    added eolimp 2164

---
+++ eolimp/src/com/ferhatelmas/eolimp/page70/q2164/Main.java
@@ -0,0 +1,20 @@
+package com.ferhatelmas.eolimp.page70.q2164;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String s = in.nextLine();
+    int n = in.nextInt();
+
+    char[] c = new char[s.length()];
+    for(int i=0; i<c.length; i++) {
+      int ord = s.charAt(i) - n;
+      if(ord < 'A') ord += 26;
+      c[i] = (char)ord;
+    }
+    System.out.println(c);
+  }
+}
