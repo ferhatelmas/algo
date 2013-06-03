c26a0503d08f69692fedff7aad3c4d67dc1b2f1c
ferhat elmas <elmas.ferhat@gmail.com>
2013-06-03 16:03:00 +0200

    added codeval closest pair

---
+++ codeEval/hard/closest_pair.py
@@ -0,0 +1,30 @@
+import sys
+from operator import itemgetter
+
+def get_distance(p, q):
+  return ((p[0]-q[0])**2 + (p[1]-q[1])**2)**0.5
+
+def find_closest_pair(ls):
+  ls = sorted(ls, key=itemgetter(0, 1))
+  dist, m = [], 40000
+  for i in range(len(ls)-1):
+    m = min(m, get_distance(ls[i], ls[i+1]))
+  if m > 10000:
+    return "INFINITY"
+  else:
+    return "%.4f" % m
+  
+points, n = [], 0
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+  if n == 0:
+    if len(points) > 0:
+      print find_closest_pair(points)
+      points = []
+    n = int(test.strip())
+    if n == 0:
+      break
+  else:
+    points.append(map(int, test.split()))
+    n -= 1
+test_cases.close()
