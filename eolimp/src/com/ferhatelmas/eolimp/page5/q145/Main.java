eedfe5d708fae78e7b1ca8d7116b3865e6bd8e09
ferhat elmas <elmas.ferhat@gmail.com>
2013-06-14 07:13:53 +0200

    added eolimp 145

---
+++ eolimp/src/com/ferhatelmas/eolimp/page5/q145/Main.java
@@ -0,0 +1,23 @@
+package com.ferhatelmas.eolimp.page5.q145;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    HashMap<String, Integer> segments = new HashMap<String, Integer>();
+    br.readLine();
+    for(String s : br.readLine().split(" ")) {
+      if(segments.containsKey(s)) segments.put(s, segments.get(s)+1);
+      else segments.put(s, 1);
+    }
+
+    int cnt = 0;
+    for(int v : segments.values())
+       cnt += v/4;
+    System.out.println(cnt);
+  }
+}
