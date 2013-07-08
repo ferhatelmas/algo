f97ef259e320f121b52540792a2899d1817fc803
ferhat elmas <elmas.ferhat@gmail.com>
2013-07-08 09:40:32 +0200

    added eolimp 542

---
+++ eolimp/src/com/ferhatelmas/eolimp/page19/q542/Main.java
@@ -0,0 +1,16 @@
+package com.ferhatelmas.eolimp.page19.q542;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int e = in.nextInt() + in.nextInt(), c = in.nextInt(), cnt = 0;
+    while(e >= c) {
+      int n = e/c;
+      cnt += n;
+      e = n + e%c;
+    }
+    System.out.println(cnt);
+  }
+}
