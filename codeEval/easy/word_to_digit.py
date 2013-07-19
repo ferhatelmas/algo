158101326a45685584429273a3315bb94cdd02c0
ferhat elmas <elmas.ferhat@gmail.com>
2013-07-19 18:21:38 +0200

    added codeval word to digit

---
+++ codeEval/easy/word_to_digit.py
@@ -0,0 +1,14 @@
+import sys
+
+mapping = ["zero", "one", "two",
+           "three", "four", "five",
+           "six", "seven", "eight", "nine"]
+d = dict(zip(mapping, xrange(10)))
+
+def get_number(ns):
+  return "".join(map(lambda n: str(d[n]), ns))
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+  print get_number(test.strip().split(";"))
+test_cases.close()
