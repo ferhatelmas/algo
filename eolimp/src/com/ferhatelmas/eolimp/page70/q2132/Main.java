a4dc7177233e0f90607b6e1dddb6ed3596473042
ferhat elmas <elmas.ferhat@gmail.com>
2013-08-21 17:54:23 +0200

    added eolimp 2(099|(13(1|2)|32(1|3)))

---
+++ eolimp/src/com/ferhatelmas/eolimp/page70/q2132/Main.java
@@ -0,0 +1,12 @@
+package com.ferhatelmas.eolimp.page70.q2132;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    double x = in.nextDouble(), y = in.nextDouble(),
+           a = in.nextDouble(), b = in.nextDouble(), c = in.nextDouble();
+    System.out.println(Math.abs(a*x + b*y + c) < 0.00001 ? "YES" : "NO");
+  }
+}
---
+++ eolimp/src/com/ferhatelmas/eolimp/page75/q2321/Main.java
@@ -0,0 +1,16 @@
+package com.ferhatelmas.eolimp.page75.q2321;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int[] ns = new int[in.nextInt()];
+    for(int i=0; i<ns.length; i++) ns[i] = in.nextInt();
+    Arrays.sort(ns);
+    StringBuilder sb = new StringBuilder();
+    for(int i : ns) sb.append(" ").append(i);
+    System.out.println(sb.substring(1));
+  }
+}
