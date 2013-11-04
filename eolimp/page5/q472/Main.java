179547350af6394ebef3144e51ebe8b46c29bb4f
ferhat elmas <elmas.ferhat@gmail.com>
2013-11-04 23:34:09 +0100

    addd eolimp 47(2|5)

---
+++ eolimp/page5/q472/Main.java
@@ -0,0 +1,10 @@
+package page5.q472;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    int n = new Scanner(System.in).nextInt();
+    System.out.println(n * (2*n - 1));
+  }
+}
