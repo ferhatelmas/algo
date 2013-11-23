cd890ef89e50944991d07ff5ec7bbdb03f6d5d59
ferhat elmas <elmas.ferhat@gmail.com>
2013-11-23 23:28:46 +0100

    added tc 17[456].2.1

---
+++ topCoder/srms/61-80/srm175/div2/clock_walk.py
@@ -0,0 +1,10 @@
+class ClockWalk:
+    def finalPosition(self, flips):
+        s = 12
+        for i, f in enumerate(flips, 1):
+            if f == 'h':
+                s += i
+            else:
+                s -= i
+            s %= 12
+        return 12 if s == 0 else s
