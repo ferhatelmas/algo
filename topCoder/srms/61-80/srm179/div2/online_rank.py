c3ed9bbeb67661fc2a361c60d366f87afe9d1773
ferhat elmas <elmas.ferhat@gmail.com>
2013-11-24 20:58:52 +0100

    added tc 17[789].2.1

---
+++ topCoder/srms/61-80/srm179/div2/online_rank.py
@@ -0,0 +1,6 @@
+class OnLineRank:
+    def calcRanks(self, k, scores):
+        def rank(l, n):
+            return 1 + sorted(l, reverse=True).index(n)
+        return sum(map(lambda (i, n): rank(scores[max(i-k, 0):i+1], n),
+                       enumerate(scores)))
