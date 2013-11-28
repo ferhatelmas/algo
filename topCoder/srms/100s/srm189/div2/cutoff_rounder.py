a5590016393fb1432a9d5b518a7879a18f368f53
ferhat elmas <elmas.ferhat@gmail.com>
2013-11-28 23:40:46 +0100

    structure change and added 497.2.1

--- topCoder/srms/100s/srm189/div2/cutoff_rounder.py
+++ topCoder/srms/100s/srm189/div2/cutoff_rounder.py
@@ -1,7 +1,6 @@
 class CutoffRounder:
     def round(self, num, cutoff):
         num = float(num.lstrip('0'))
-        print num - int(num)
         if num - int(num) >= float(cutoff):
             return int(num) + 1
         return int(num)
