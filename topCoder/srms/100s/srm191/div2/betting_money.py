52b707d2faac7cc0cd054c744e2a179879326763
ferhat elmas <elmas.ferhat@gmail.com>
2013-11-29 22:20:57 +0100

    added tc 19[0124].2.1

---
+++ topCoder/srms/100s/srm191/div2/betting_money.py
@@ -0,0 +1,4 @@
+class BettingMoney:
+    def moneyMade(self, amounts, centsPerDollar, finalResult):
+        return sum(map(lambda (i, (a, c)): -a*c if finalResult == i else a*100,
+                       enumerate(zip(amounts, centsPerDollar))))
---
+++ topCoder/srms/100s/srm192/div2/box_loader.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+class BoxLoader:
+    def mostItems(self, boxX, boxY, boxZ, itemX, itemY, itemZ):
+        def c(box, item):
+            return reduce(lambda a, (b, i): a*(b/i), zip(box, item), 1)
+        box, item = [boxX, boxY, boxZ], [itemX, itemY, itemZ]
+        return max(map(lambda i: c(box, list(i)), permutations(item)))
