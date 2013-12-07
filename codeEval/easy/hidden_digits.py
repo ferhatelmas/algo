d813b23abf3e58aafae802294a59b8577abc7bd1
ferhat elmas <elmas.ferhat@gmail.com>
2013-12-07 22:31:45 +0100

    added codeval hidden_digits

---
+++ codeEval/easy/hidden_digits.py
@@ -0,0 +1,15 @@
+import sys
+
+def unhide(s):
+    r = ''
+    for e in s:
+        if 'a' <= e <= 'j':
+            r += str(0 + ord(e) - ord('a'))
+        elif '0' <= e <= '9':
+            r += e
+    return r if r else 'NONE'
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    print unhide(test)
+test_cases.close()
