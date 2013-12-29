737ad8bcc3260a1d08f708a717ab5920a5227805
ferhat elmas <elmas.ferhat@gmail.com>
2013-12-29 06:26:58 +0100

    added uri comp in py

--- codeEval/medium/uri_comparison.rb
+++
@@ -1,7 +0,0 @@
-require 'uri'
-File.open(ARGV[0]).each_line do |l|
-  v = l.strip.split(';').collect! do |u|
-    URI(URI.unescape(u)).normalize.to_s
-  end.to_a.uniq.length
-  puts ['False', 'True'][v%2]
-end
