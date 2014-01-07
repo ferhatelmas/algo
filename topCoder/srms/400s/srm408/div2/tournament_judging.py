9a0118f9fc595b85756b5e0a13ecf1111a1642fd
ferhat elmas <elmas.ferhat@gmail.com>
2014-01-08 00:18:02 +0100

    added tc 40[6-10].2.1

---
+++ topCoder/srms/400s/srm408/div2/tournament_judging.py
@@ -0,0 +1,3 @@
+class TournamentJudging:
+    def getPoints(self, rawScores, conversionFactor):
+        return sum([int(round(float(r)/c)) for r, c in zip(rawScores, conversionFactor)])
---
+++ topCoder/srms/400s/srm409/div2/stick.py
@@ -0,0 +1,14 @@
+from heapq import heappop, heappush
+
+class Stick:
+    def pieces(self, x):
+        s, l, c = 64, [64], 1
+        while s > x:
+            m = heappop(l) / 2
+            heappush(l, m)
+            s -= m
+            if s < x:
+                s += m
+                heappush(l, m)
+                c += 1
+        return c
