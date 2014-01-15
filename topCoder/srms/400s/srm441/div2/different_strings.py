6a6dedeb514820e653a7cb5aa816acea94a609da
ferhat elmas <elmas.ferhat@gmail.com>
2014-01-15 10:24:42 +0100

    added tc 44[01].2.1

---
+++ topCoder/srms/400s/srm441/div2/different_strings.py
@@ -0,0 +1,6 @@
+class DifferentStrings:
+    def minimize(self, A, B):
+        def diff(s):
+            return sum(map(lambda (a, b): a != b, zip(A, s)))
+        la = len(A)
+        return min(diff(B[i:i+la]) for i in xrange(len(B)-la+1))
