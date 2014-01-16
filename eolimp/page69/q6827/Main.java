73f7d244710972dd376d369297d17ce925bdbd79
ferhat elmas <elmas.ferhat@gmail.com>
2014-01-16 23:41:30 +0100

    added eolimp 6827

---
+++ eolimp/page69/q6827/Main.java
@@ -0,0 +1,10 @@
+package page69.q6827;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println(in.nextLine().length() >= in.nextLine().length() ? "go" : "no");
+  }
+}
