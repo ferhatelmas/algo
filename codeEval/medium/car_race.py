4856c8345abece2fc55dace476a6583bdeececa9
ferhat elmas <elmas.ferhat@gmail.com>
2014-04-08 16:22:50 +0200

    added codeval car race

---
+++ codeEval/medium/car_race.py
@@ -0,0 +1,44 @@
+import sys
+
+class Car:
+    def __init__(self, i, m, a, b):
+        self.i = int(i)
+        self.m = m / 3600
+        self.a = a
+        self.b = b
+
+    def calculate_lap_time(self, t, total_track):
+        total, path = self.a, (self.m * self.a) /2.0
+        for l, d in t:
+            n = (self.m * (180-d)) / 180.0
+            t1 = ((self.m - n) * self.b) / self.m
+            total += t1
+            l1 = ((self.m + n) * t1) / 2.0
+            path += l1
+
+            t2 = ((self.m - n) * self.a) / self.m
+            total += t2
+            l2 = ((self.m + n) * t2) / 2.0
+            path += l2
+        total += (total_track - path) / self.m
+        self.lap_time = total
+
+    def __cmp__(self, o):
+        return cmp(self.lap_time, o.lap_time)
+
+    def __str__(self):
+        return '{} {:.2f}'.format(self.i, self.lap_time)
+
+cars = []
+test_cases = open(sys.argv[1], 'r')
+for i, test in enumerate(test_cases):
+  if i:
+    cars.append(Car(*map(float, test.split())))
+  else:
+    track = map(float, test.split())
+    track = zip(track[0::2], track[1::2])
+test_cases.close()
+
+total = sum(t[0] for t in track)
+map(lambda c: c.calculate_lap_time(track, total), cars)
+print '\n'.join(map(str, sorted(cars)))
