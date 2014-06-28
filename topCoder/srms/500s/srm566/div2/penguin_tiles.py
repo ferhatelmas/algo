2398da01dfa18e31dc08c302dd642c145fe70ef9
ferhat elmas <elmas.ferhat@gmail.com>
2014-06-29 00:02:17 +0200

    added srm56[3-7].2.1

---
+++ topCoder/srms/500s/srm566/div2/penguin_tiles.py
@@ -0,0 +1,8 @@
+class PenguinTiles:
+    def minMoves(self, tiles):
+        rc, cc = len(tiles), len(tiles[0])
+        for i, r in enumerate(tiles):
+            for j, c in enumerate(r):
+                if c == '.':
+                    return (rc-1 != i) + (cc-1 != j)
+        return 0
