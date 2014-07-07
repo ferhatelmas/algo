023670141875b653d00eec4bc9da93b4ce35e2a1
ferhat elmas <elmas.ferhat@gmail.com>
2014-07-07 16:58:03 +0200

    added eolimp 2811

---
+++ eolimp/page29/q2811/Main.java
@@ -0,0 +1,10 @@
+package page29.q2811;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    int n = new Scanner(System.in).nextInt();
+    System.out.println((n * (n+1) / 2) % 2);
+  }
+}
