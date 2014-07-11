02812e34bc3c2b16aa76cd064f7f68b2f69a2ec9
ferhat elmas <elmas.ferhat@gmail.com>
2014-07-11 21:43:10 +0200

    added codeval age distribution

---
+++ codeEval/easy/age_distribution.py
@@ -0,0 +1,13 @@
+import sys
+from bisect import bisect
+
+def distribute(age):
+    s = ('Home', 'Preschool', 'Elementary school', 'Middle school', 'High school',
+         'College', 'Work', 'Retirement', 'This program is for humans')
+    a = [0, 3, 5, 12, 15, 19, 23, 66, 101]
+    return s[bisect(a, age)-1]
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    print distribute(int(test.rstrip()))
+test_cases.close()
