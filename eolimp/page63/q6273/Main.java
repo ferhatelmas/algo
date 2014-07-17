9c9744a19c8e44587394d52fc16f42759130311f
ferhat elmas <elmas.ferhat@gmail.com>
2014-07-17 16:00:43 +0200

    added eolimp 6273

---
+++ eolimp/page63/q6273/Main.java
@@ -0,0 +1,12 @@
+package page63.q6273;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println((int)Math.sqrt(Math.pow(in.nextInt(), 2) +
+                                      Math.pow(in.nextInt(), 2) +
+                                      Math.pow(in.nextInt(), 2)));
+  }
+}
