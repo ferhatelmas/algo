29fb386a0b78509de3c7700ee66d84099a5eb6ab
ferhat elmas <elmas.ferhat@gmail.com>
2014-07-19 23:31:46 +0200

    added eolimp 1287

---
+++ eolimp/page13/q1287/Main.java
@@ -0,0 +1,10 @@
+package page13.q1287;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println(in.nextLong() * in.nextLong());
+  }
+}
