ebcb34a572b3ca729dc770765d9d139e08d26861
ferhat elmas <elmas.ferhat@gmail.com>
2014-07-26 05:12:24 +0200

    added eolimp 846

---
+++ eolimp/page9/q846/Main.java
@@ -0,0 +1,11 @@
+package page9.q846;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println(new BigInteger(in.next()).multiply(new BigInteger(in.next())).toString());
+  }
+}
