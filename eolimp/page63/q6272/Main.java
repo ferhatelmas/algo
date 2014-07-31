ca34599c2850eed8267a96d1657ccc5e7b0fbc7d
ferhat elmas <elmas.ferhat@gmail.com>
2014-07-31 03:21:43 +0200

    added eolimp 6272

---
+++ eolimp/page63/q6272/Main.java
@@ -0,0 +1,11 @@
+package page63.q6272;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    System.out.println(in.nextInt() + in.nextInt() + in.nextInt() - n);
+  }
+}
