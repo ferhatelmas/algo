18676289e498c169aad1dfa186c55760f8e99dc5
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-01 07:29:36 +0200

    added eolimp 5868

---
+++ eolimp/page59/q5868/Main.java
@@ -0,0 +1,10 @@
+package page59.q5868;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println(in.nextInt() ^ in.nextInt());
+  }
+}
