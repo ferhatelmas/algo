f50f2e7f03428b3bd120e129079aee7aedd24384
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-06 18:04:48 +0200

    added eolimp 4743

---
+++ eolimp/page48/q4743/Main.java
@@ -0,0 +1,9 @@
+package page48.q4743;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println((int)Math.pow(2, new Scanner(System.in).nextInt())-1);
+  }
+}
