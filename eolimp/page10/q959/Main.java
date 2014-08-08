c65d8aa4ec509e21bd1e511cfc01174f77e4385d
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-08 08:22:25 +0200

    added eolimp 959

---
+++ eolimp/page10/q959/Main.java
@@ -0,0 +1,10 @@
+package page10.q959;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    int n = new Scanner(System.in).nextInt();
+    System.out.println(n / 1000 + n %10);
+  }
+}
