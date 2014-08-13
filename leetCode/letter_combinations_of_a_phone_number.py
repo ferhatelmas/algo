b01e4d89294a2b01961364246efe733374700c14
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-13 10:50:50 +0200

    added lc letter combinations

---
+++ leetCode/letter_combinations_of_a_phone_number.py
@@ -0,0 +1,18 @@
+from itertools import product
+
+class Solution:
+
+    m = {
+        '1': '',
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
+
+    def letterCombinations(self, digits):
+        return list(''.join(e) for e in product(*(self.m[d] for d in digits)))
