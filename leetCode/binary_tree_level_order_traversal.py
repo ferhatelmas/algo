d18ee4c9c19065943ffa23a80ed85039cd65527b
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-13 05:57:12 +0200

    added leetcode binary tree level order traversal

---
+++ leetCode/binary_tree_level_order_traversal.py
@@ -0,0 +1,12 @@
+from itertools import izip_longest
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrder(self, root):
+        if root is None:
+            return []
+        return [[root.val]] + [a + b
+                               for a, b in izip_longest(self.levelOrder(root.left),
+                                                        self.levelOrder(root.right),
+                                                        fillvalue=[])]
