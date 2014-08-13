a68c73e4f6762eca7f604c378ab463ae71967822
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-13 05:59:35 +0200

    added leetcode binary tree inorder traversal

---
+++ leetCode/binary_tree_inorder_traversal.py
@@ -0,0 +1,5 @@
+class Solution:
+    def inorderTraversal(self, root):
+        if root is None:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
