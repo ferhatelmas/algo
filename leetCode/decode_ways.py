b583d36c275d25238aeaf2464efe413df96b5246
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-14 13:08:44 +0200

    added lc decode ways

---
+++ leetCode/decode_ways.py
@@ -0,0 +1,21 @@
+class Solution:
+    m = {}
+
+    def numDecodings(self, s):
+        if not s:
+            return 0
+        return self.decode(s, len(s))
+
+    def decode(self, s, l):
+        if s[:1] == '0':
+            return 0
+        if l < 2:
+            return 1
+        if s in self.m:
+            return self.m[s]
+        n = self.decode(s[1:], l-1)
+        if 0 < int(s[:2]) <= 26:
+            n += self.decode(s[2:], l-2)
+        self.m[s] = n
+        return n
+
