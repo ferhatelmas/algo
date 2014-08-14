cc022399f1a4d50ad46ff4235503f478a41b2e21
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-14 12:54:58 +0200

    addet lc string to integer

---
+++ leetCode/string_to_integer.py
@@ -0,0 +1,13 @@
+import re
+
+class Solution:
+    def atoi(self, str):
+        try:
+            n = int(re.split(r'[^\d\-\+]', str.strip())[0])
+            if n > 2147483647:
+                return 2147483647
+            elif n < -2147483648:
+                return -2147483648
+            return n
+        except:
+            return 0
