e1cc71519e5cb9ffbc5dd3ddab954a896e007bcb
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-18 08:24:15 +0200

    added timus 1083

---
+++ timus/vol1/1083.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	var s string
+	fmt.Scan(&n)
+	fmt.Scan(&s)
+	r, k := 1, len(s)
+	for i := n; i > 0; i-=k {
+		r *= i
+	}
+	fmt.Println(r)
+}
