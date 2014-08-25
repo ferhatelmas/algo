fdd45b13ab08d4d36b5104b48c7abb587ae1a2f4
ferhat elmas <elmas.ferhat@gmail.com>
2014-08-25 07:22:08 +0200

    added leetcode roman to int

---
+++ leetCode/roman_to_integer.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    ns = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+
+    def romanToInt(self, s):
+        s = (
+            s.replace('CM', 'DCCCC')
+             .replace('CD', 'CCCC')
+             .replace('XC', 'LXXXX')
+             .replace('XL', 'XXXX')
+             .replace('IX', 'VIIII')
+             .replace('IV', 'IIII')
+        )
+        return sum(self.ns[e] for e in s)
