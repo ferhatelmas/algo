60a4db114b5c97e4a5b6158bd4dc652751060ae7
ferhat elmas <elmas.ferhat@gmail.com>
2014-09-03 03:48:07 +0200

    added eolimp 5322

---
+++ eolimp/page54/q5322/Main.java
@@ -0,0 +1,11 @@
+package page54.q5322;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    BigInteger b = new BigInteger(new Scanner(System.in).next(), 2);
+    System.out.println(b.toString(16).toUpperCase());
+  }
+}
