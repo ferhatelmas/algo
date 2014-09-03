ff7312654af83d51f0bfba86690c9044c0f56342
ferhat elmas <elmas.ferhat@gmail.com>
2014-09-03 02:14:13 +0200

    added eolimp 22

--- eolimp/page1/q22/Main.java
+++ eolimp/page1/q22/Main.java
@@ -1,27 +1,32 @@
 package page1.q22;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
   public static void main(String[] args) {
-
     Scanner in=new Scanner(System.in);
     int a = in.nextInt(), b = in.nextInt(), cnt = 0;
 
-    for(int i=Math.min(a, b); i<=Math.max(a, b); i++) {
-      if(isPrime(i) && isPrime(Integer
-          .parseInt(new StringBuilder(String.valueOf(i)).reverse().toString())))
-        cnt++;
-    }
+    boolean p[] = new boolean[10001];
+    Arrays.fill(p, true); p[1] = false;
+    for(int i=2; i<p.length; i++)
+      if(p[i])
+        for(int j=2*i; j<p.length; j+=i)
+          p[j] = false;
+
+    for(int i=Math.min(a, b), k=Math.max(a, b); i<=k; i++)
+      if(p[i] && p[reverse(i)]) cnt++;
 
     System.out.println(cnt);
   }
 
-  private static boolean isPrime(int a) {
-    if(a < 2) return false;
-    for(int i=2; i<=Math.sqrt(a); i++) if(a%i==0) return false;
-    return true;
+  private static int reverse(int n) {
+    int r = 0;
+    while(n > 0) {
+      r = 10 * r + n % 10;
+      n /= 10;
+    }
+    return r;
   }
-
 }
