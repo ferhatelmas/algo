3a6f0f6d56b204bd22a62c4b3d38e87d22241524
ferhat elmas <elmas.ferhat@gmail.com>
2014-09-06 16:27:37 +0200

    added eolimp 5050

---
+++ eolimp/page51/q5050/Main.java
@@ -0,0 +1,9 @@
+package page51.q5050;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(1 << (new Scanner(System.in).nextInt()));
+  }
+}
