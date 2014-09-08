a9c3d77f33760549289794c7a07a9a691f301cf3
ferhat elmas <elmas.ferhat@gmail.com>
2014-09-08 04:40:50 +0200

    added eolimp 4718

---
+++ eolimp/page48/q4718/Main.java
@@ -0,0 +1,9 @@
+package page48.q4718;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, " + new Scanner(System.in).next());
+  }
+}
