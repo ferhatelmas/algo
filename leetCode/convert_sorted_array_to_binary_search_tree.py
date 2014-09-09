c799bc80466cce05add0d4a69ca7ad90abdbaff7
ferhat elmas <elmas.ferhat@gmail.com>
2014-09-09 04:30:12 +0200

    added lc convert sorted array into bst

---
+++ leetCode/convert_sorted_array_to_binary_search_tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortedArrayToBST(self, num):
+        if not num:
+            return None
+        l = len(num)
+        if l == 1:
+            return TreeNode(num[0])
+        t = TreeNode(num[l/2])
+        t.left = self.sortedArrayToBST(num[:l/2])
+        t.right = self.sortedArrayToBST(num[l/2+1:])
+        return t
