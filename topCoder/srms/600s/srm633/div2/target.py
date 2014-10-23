f95ffa17a301eea4ee08590080841884dd98aaf4
ferhat elmas <elmas.ferhat@gmail.com>
2014-10-23 06:01:43 +0200

    added tc 633.2.1

---
+++ topCoder/srms/600s/srm633/div2/target.py
@@ -0,0 +1,13 @@
+class Target:
+    def draw(self, n):
+        ls, p, wall = [], n/2, ('#', ' ')
+        def middle(i, r=1):
+            return ('#' + wall[i%2])[::r] if i != p else ''
+
+        for i in xrange(n):
+            j = (n/2 - abs(n/2 - i)) / 2 
+            s = '# ' * j + middle(i)
+            s += wall[i%2] * (n - 2 * len(s))
+            s += middle(i, -1) + ' #' * j
+            ls.append(s)
+        return ls
