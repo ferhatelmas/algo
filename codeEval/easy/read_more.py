bff85d632386c1dc009af4c9585433a459b5375d
ferhat elmas <elmas.ferhat@gmail.com>
2014-10-24 20:24:24 +0200

    added codeval read more

---
+++ codeEval/easy/read_more.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+
+def transform(s):
+    if len(s) <= 55:
+        return s
+    a = s[:40]
+    if re.match(r'\S.*', s[40:]):
+        i = a.rfind(' ')
+        if i > -1:
+            a = a[:i]
+    return a.rstrip(' ') + '... <Read More>'
+
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    print transform(test.rstrip())
+test_cases.close()
