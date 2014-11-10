1f2e2888ec4f6cc227e46367ccfe03378e29e611
ferhat elmas <elmas.ferhat@gmail.com>
2014-11-10 12:57:47 +0100

    added eolimp 7293

---
+++ eolimp/page73/q7293/Main.java
@@ -0,0 +1,9 @@
+package page73.q7293;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(145 - new Scanner(System.in).nextInt());
+  }
+}
