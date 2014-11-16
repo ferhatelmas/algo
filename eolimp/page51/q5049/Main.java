b6ef560c8173dc0fe6cccabd903b6de879750a3b
ferhat elmas <elmas.ferhat@gmail.com>
2014-11-17 00:37:21 +0100

    added eolimp 5049

---
+++ eolimp/page51/q5049/Main.java
@@ -0,0 +1,9 @@
+package page51.q5049;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(new Scanner(System.in).nextLine().replaceAll("\\s+", " "));
+  }
+}
