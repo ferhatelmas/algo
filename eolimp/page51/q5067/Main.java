a5b084a4974f2e18309703e06146daabc4fff249
ferhat elmas <elmas.ferhat@gmail.com>
2014-11-16 23:57:10 +0100

    added eolimp 5067

---
+++ eolimp/page51/q5067/Main.java
@@ -0,0 +1,15 @@
+package page51.q5067;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    BigInteger b = new BigInteger(in.nextLine()),
+               c = new BigInteger(in.nextLine());
+    BigInteger r[] = b.divideAndRemainder(c);
+    System.out.println(r[0].toString());
+    System.out.println(r[1].toString());
+  }
+}
