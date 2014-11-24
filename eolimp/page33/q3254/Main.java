2acb6647a4dc95f280007ff8608a764985beb62d
ferhat elmas <elmas.ferhat@gmail.com>
2014-11-24 21:05:41 +0100

    added eolimp 3254

---
+++ eolimp/page33/q3254/Main.java
@@ -0,0 +1,9 @@
+package page33.q3254;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Integer.parseInt(new Scanner(System.in).nextLine(), 2));
+  }
+}
