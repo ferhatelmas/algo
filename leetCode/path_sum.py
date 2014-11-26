08c1d78f5074a57c015cdb36c2cfc0abb5510fa9
ferhat elmas <elmas.ferhat@gmail.com>
2014-11-26 10:35:29 +0100

    added lc path sum

---
+++ leetCode/path_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasPathSum(self, root, sum):
+        def hasSum(root, acc):
+            if root is None:
+                return False
+            elif root.left is None and root.right is None:
+                return acc + root.val == sum
+            return hasSum(root.left, root.val + acc) or hasSum(root.right, root.val + acc)
+        return hasSum(root, 0)
