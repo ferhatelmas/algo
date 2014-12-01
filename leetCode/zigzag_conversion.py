7843ef0e82d46c12644d443981bab2a0c84087f2
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-01 21:57:19 +0100

    added lc zigzag conversion

---
+++ leetCode/zigzag_conversion.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convert(self, s, nRows):
+        if nRows == 1:
+            return s
+        ls, i, j = [[] for _ in xrange(nRows)], 0, 1
+        for e in s:
+            ls[i].append(e)
+            i += j
+            if i == nRows - 1:
+                j = -1
+            elif i == 0:
+                j = 1
+        return ''.join(''.join(l) for l in ls)
