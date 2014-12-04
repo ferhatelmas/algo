19d7713c5f5ef7bcd68dbf1015ea5480ab98eff7
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-04 22:28:52 +0100

    added lc pascal triangle 1

---
+++ leetCode/pascals_triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows):
+        if numRows == 0:
+            return []
+        ls = [[1]]
+        for _ in xrange(numRows-1):
+            el = [1]
+            el.extend([a+b for a, b in zip(ls[-1], ls[-1][1:])])
+            el.append(1)
+            ls.append(el)
+        return ls
