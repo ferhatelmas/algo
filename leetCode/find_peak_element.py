65002dfe854048313ba89058255b77e7a02c0d81
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-09 22:41:49 +0100

    added lc find peak element

---
+++ leetCode/find_peak_element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findPeakElement(self, num):
+        l = len(num) - 1
+        for i, e in enumerate(num):
+            if((i == 0 or e > num[i-1]) and (i == l or e > num[i+1])):
+                return i
+        return 0
