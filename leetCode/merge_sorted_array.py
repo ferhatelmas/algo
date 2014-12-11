ff12f9759af78316749e864ae37094069e1c8651
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-11 23:32:10 +0100

    added lc merge sorted array

---
+++ leetCode/merge_sorted_array.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def merge(self, A, m, B, n):
+        sm, sn, i = m - 1, n - 1, m + n - 1
+
+        while sm >= 0 and sn >= 0:
+            if A[sm] > B[sn]:
+                A[i] = A[sm]
+                sm -= 1
+            else:
+                A[i] = B[sn]
+                sn -= 1
+            i -= 1
+        while sn >= 0:
+            A[i] = B[sn]
+            i -= 1
+            sn -= 1
