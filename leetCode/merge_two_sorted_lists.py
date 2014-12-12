ab1d46b9218265398df92d2cc87d9bfb9ad9230e
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-12 11:53:33 +0100

    added lc merge two sorted lists

---
+++ leetCode/merge_two_sorted_lists.py
@@ -0,0 +1,28 @@
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+
+    def mergeTwoLists(self, l1, l2):
+        ls = p = ListNode(0)
+        while l1 is not None and l2 is not None:
+            if l1.val < l2.val:
+                p.next = l1
+                l1 = l1.next
+            else:
+                p.next = l2
+                l2 = l2.next
+            p = p.next
+        while l1 is not None:
+            p.next = l1
+            p = p.next
+            l1 = l1.next
+        while l2 is not None:
+            p.next = l2
+            p = p.next
+            l2 = l2.next
+        return ls.next
