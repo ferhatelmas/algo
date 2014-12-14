10e8e490f80905424a526fd201eebc65a6dc5efa
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-14 22:29:20 +0100

    added lc longest common prefix

---
+++ leetCode/longest_common_prefix.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        ls = []
+        for i, e in enumerate(min(strs, key=len)):
+            if [s for s in strs if s[i] != e]:
+                break
+            ls.append(e)
+        return "".join(ls)
