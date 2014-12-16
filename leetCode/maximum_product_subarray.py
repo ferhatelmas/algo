cbcf6d02a2e4d67f0cdcb5a039e72934ec447976
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-16 23:51:02 +0100

    added lc maximum product subarray

---
+++ leetCode/maximum_product_subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+
+    def maxProduct(self, A):
+        a = b = r = A[0]
+        for x in A[1:]:
+            a, b = max(x, a * x, b * x), min(x, a * x, b * x)
+            r = max(r, a)
+        return r
