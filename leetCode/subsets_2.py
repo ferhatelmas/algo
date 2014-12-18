dc9b035ab14910e8d1706098a9a92fa732b8d40f
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-18 23:46:32 +0100

    added lc subsets 2

---
+++ leetCode/subsets_2.py
@@ -0,0 +1,15 @@
+from itertools import chain, combinations
+
+
+class Solution:
+
+    def subsetsWithDup(self, S):
+        return [
+            list(t)
+            for t in sorted(set(
+                tuple(sorted(e))
+                for e in chain(*[
+                    combinations(S, i)
+                    for i in range(len(S) + 1)
+                ]
+        )))]
