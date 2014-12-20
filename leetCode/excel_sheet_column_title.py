d6253c183bc42054e47e71fe075540cae8a00c69
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-20 22:29:57 +0100

    added lc excel sheet column title

---
+++ leetCode/excel_sheet_column_title.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def convertToTitle(self, num):
+        ls = []
+        while num:
+            num, i = divmod(num, 26)
+            if i == 0 and num > 0:
+                num -= 1
+                i = 26
+            ls.append(chr(ord('A') - 1 + i))
+        return ''.join(reversed(ls))
