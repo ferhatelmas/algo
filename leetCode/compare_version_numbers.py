3826261488bda8c1a6058d7c1901b817971b82ac
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-21 19:29:58 +0100

    added lc compare version numbers

---
+++ leetCode/compare_version_numbers.py
@@ -0,0 +1,11 @@
+from itertools import izip_longest
+
+
+class Solution:
+
+    def compareVersion(self, version1, version2):
+        f = lambda v: tuple(map(int, v.split('.')))
+        for a, b in izip_longest(f(version1), f(version2), fillvalue=0):
+            if a != b:
+                return cmp(a, b)
+        return 0
