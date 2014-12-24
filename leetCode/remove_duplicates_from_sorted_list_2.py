a10420152d91265c93eb2990c261f868d0c21fbe
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-24 19:32:10 +0100

    added lc remove dups from sorted list 2

---
+++ leetCode/remove_duplicates_from_sorted_list_2.py
@@ -0,0 +1,25 @@
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        return '{}->{}'.format(self.val, self.next)
+
+
+class Solution:
+
+    def deleteDuplicates(self, head):
+        p = h = ListNode(0)
+        while head is not None:
+            if head.next is not None and head.val == head.next.val:
+                v = head.val
+                while head is not None and head.val == v:
+                    head = head.next
+            else:
+                p.next = head
+                p = p.next
+                head = head.next
+        p.next = None
+        return h.next
