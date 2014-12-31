9fc0bfd2cb92a5d7f92487f35d321c240e5ffd24
ferhat elmas <elmas.ferhat@gmail.com>
2014-12-31 19:32:10 +0100

    added lc binary search tree iterator

---
+++ leetCode/binary_search_tree_iterator.py
@@ -0,0 +1,34 @@
+class TreeNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class BSTIterator:
+
+    def __init__(self, root):
+        self.s = []
+        c = root
+        while c is not None:
+            self.s.append(c)
+            if c.left is not None:
+                c = c.left
+            else:
+                break
+
+    def hasNext(self):
+        return bool(self.s)
+
+    def next(self):
+        n = c = self.s.pop()
+        if c.right is not None:
+            c = c.right
+            while c is not None:
+                self.s.append(c)
+                if c.left is not None:
+                    c = c.left
+                else:
+                    break
+        return n.val
