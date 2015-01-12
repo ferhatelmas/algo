b2b61b900d25e2b63d2ef3a2dfd75b4e132d8e2e
ferhat elmas <elmas.ferhat@gmail.com>
2015-01-12 14:26:16 +0100

    added lc alg unique paths

---
+++ leetCode/unique_paths.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def uniquePaths(self, m, n):
+        r = [[0] * n for _ in xrange(m)]
+        r[0][0] = 1
+        for i in xrange(m):
+            for j in xrange(n):
+                if i:
+                    r[i][j] += r[i - 1][j]
+                if j:
+                    r[i][j] += r[i][j - 1]
+        return r[m - 1][n - 1]
