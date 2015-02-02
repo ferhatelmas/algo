8602c67a6a6aa1b4070c11d1fce7577e1d6802a4
ferhat elmas <elmas.ferhat@gmail.com>
2015-02-02 23:43:29 +0100

    added chef buy1get1

---
+++ codeChef/practice/easy/buy1get1.py
@@ -0,0 +1,9 @@
+from sys import stdin
+from itertools import groupby
+
+for i, s in enumerate(stdin):
+    if i:
+        print sum(
+            (l / 2) + (l % 2)
+            for l in (len(list(g)) for k, g in groupby(sorted(s.rstrip())))
+        )
