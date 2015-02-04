7db0287e587534ef371501038e8acbfc223fcd14
ferhat elmas <elmas.ferhat@gmail.com>
2015-02-04 23:58:10 +0100

    added chef lecandy

---
+++ codeChef/practice/easy/lecandy.py
@@ -0,0 +1,10 @@
+from sys import stdin
+
+
+for i, ln in enumerate(stdin):
+    if i == 0:
+        continue
+    if i % 2:
+        _, c = map(int, ln.split())
+    else:
+        print('Yes', 'No')[c < sum(map(int, ln.split()))]
