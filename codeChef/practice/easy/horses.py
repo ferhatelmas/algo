745bcf3344b23d265c39e0fb36a107d972f10e85
ferhat elmas <elmas.ferhat@gmail.com>
2015-02-06 20:00:00 +0100

    aded chef horses

---
+++ codeChef/practice/easy/horses.py
@@ -0,0 +1,10 @@
+from sys import stdin
+
+
+for i, ln in enumerate(stdin):
+    if i > 0 and i % 2 == 0:
+        ls = sorted(map(int, ln.split()))
+        print min(
+            b - a
+            for a, b in zip(ls, ls[1:])
+        )
