b15f6750ce7edcd36f42142d44049d0642c3bbd1
ferhat elmas <elmas.ferhat@gmail.com>
2015-02-15 04:25:51 +0100

    added chef stones

---
+++ codeChef/practice/easy/stones.py
@@ -0,0 +1,9 @@
+from sys import stdin
+
+
+for i, ln in enumerate(stdin):
+    if i:
+        if i % 2 == 1:
+            s = set(ln.rstrip())
+        else:
+            print sum(e in s for e in ln.rstrip())
