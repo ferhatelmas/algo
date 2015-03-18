05377d22ffc17d2f77436f9405981e6cb141f5c5
ferhat elmas <elmas.ferhat@gmail.com>
2015-03-18 23:10:16 +0100

    added chef totr

---
+++ codeChef/practice/easy/totr.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+
+alphabet = None
+
+
+def translate(s):
+    ls = []
+    for e in s:
+        if e == '_':
+            ls.append(' ')
+        elif 'a' <= e <= 'z':
+            ls.append(alphabet[e])
+        elif 'A' <= e <= 'Z':
+            ls.append(alphabet[e.lower()].upper())
+        else:
+            ls.append(e)
+    return ''.join(ls)
+
+
+for i, ln in enumerate(stdin):
+    if i == 0:
+        alphabet = dict(zip('abcdefghijklmnopqrstuvwxyz', ln.split()[1]))
+    else:
+        print(translate(ln.rstrip()))
