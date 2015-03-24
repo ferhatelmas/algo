f1a0b81a49c70a7eda9d5d7029966fe5dced7d8d
ferhat elmas <elmas.ferhat@gmail.com>
2015-03-24 23:58:10 +0100

    added he prom night

---
+++ hackerEarth/practice_problems/easy/PromNight.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+for i, ln in enumerate(stdin, start=-1):
+        if i == -1:
+            continue
+        c = i % 3
+        if c == 0:
+            n, m = map(int, ln.split())
+        elif c == 1:
+            a = map(int, ln.split())
+        else:
+            b = map(int, ln.split())
+            print('NO', 'YES')[
+                all(x > y for x, y in zip(sorted(a), sorted(b)))
+            ]
