efc55f16fce9c54bfc3d4dbede30dfb3f39b10fd
ferhat elmas <elmas.ferhat@gmail.com>
2015-04-17 22:00:38 +0200

    added codeval compare points

--- codeEval/easy/compare_points.py
+++ codeEval/easy/compare_points.py
@@ -2,27 +2,10 @@ import sys
 
 
 def get_direction(o, p, r, q):
-    if o == r and p == q:
+    a, b = 'hSN'[cmp(p, q)], 'hWE'[cmp(o, r)]
+    if a == b:
         return 'here'
-    elif p < q:
-        if o < r:
-            return 'NE'
-        elif o > r:
-            return 'NW'
-        else:
-            return 'N'
-    elif p == q:
-        if o < r:
-            return 'E'
-        else:
-            return 'W'
-    elif p > q:
-        if o < r:
-            return 'SE'
-        elif o > r:
-            return 'SW'
-        else:
-            return 'S'
+    return (a + b).replace('h', '')
 
 
 with open(sys.argv[1], 'rb') as test_cases:
