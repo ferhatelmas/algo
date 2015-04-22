d8237afe66d63fc9701cb570168348b2e38d30cb
ferhat elmas <elmas.ferhat@gmail.com>
2015-04-22 20:19:09 +0200

    lc bitwise and numbers range

---
+++ leetCode/algorithms/medium/bitwise_and_of_numbers_range.py
@@ -0,0 +1,13 @@
+import math
+
+
+class Solution:
+    def rangeBitwiseAnd(self, m, n):
+        if m == n:
+            return m
+        diff = int(math.log(n - m) / math.log(2) + 1)
+        m >>= diff
+        n >>= diff
+        return (m & n) << diff
+
+print(Solution().rangeBitwiseAnd(5, 7))
