ddc98dc0a79b81f00fd13f8d51f0167c7e2daf37
ferhat elmas <elmas.ferhat@gmail.com>
2015-04-25 02:21:01 +0200

    lc symmetric tree

---
+++ leetCode/algorithms/easy/symmetric_tree.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = self.right = None
+
+
+class Solution:
+    def isSymmetric(self, root):
+        def isSym(left, right):
+            if left is None or right is None:
+                return left == right
+            if left.val != right.val:
+                return False
+            return (
+                isSym(left.left, right.right) and
+                isSym(left.right, right.left)
+            )
+        return isSym(root, root)
+
