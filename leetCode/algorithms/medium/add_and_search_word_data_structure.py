7662550d8536a84ae39f06dd6f316cb8523c87c1
ferhat elmas <elmas.ferhat@gmail.com>
2015-05-23 23:44:53 +0200

    leetcode word search tle

---
+++ leetCode/algorithms/medium/add_and_search_word_data_structure.py
@@ -0,0 +1,29 @@
+# TODO: make a trie
+from string import lowercase
+from itertools import product
+
+
+class WordDictionary:
+    def __init__(self):
+        self.words = set()
+
+    def addWord(self, word):
+        self.words.add(word)
+
+    def search(self, word):
+        return any(
+            ''.join(e) in self.words
+            for e in product(*(
+                [e] if e else list(lowercase)
+                for e in word.split('.')
+            ))
+        )
+
+w = WordDictionary()
+w.addWord('bad')
+w.addWord('dad')
+w.addWord('mad')
+assert not w.search('pad')
+assert w.search('bad')
+assert w.search('.ad')
+assert w.search('b..')
