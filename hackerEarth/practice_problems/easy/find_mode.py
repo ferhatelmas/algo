872849e129722b6c5bb3e7ef68dfeaa54118ef45
ferhat elmas <elmas.ferhat@gmail.com>
2015-05-27 21:07:16 +0200

    hacker earth find mode

---
+++ hackerEarth/practice_problems/easy/find_mode.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+
+for _ in xrange(int(raw_input())):
+    raw_input()
+    c = Counter(raw_input().rstrip().split())
+    m = max(c.values())
+    res = (k for k, v in c.items() if v == m)
+    print(' '.join(sorted(res, reverse=True, key=int)))
