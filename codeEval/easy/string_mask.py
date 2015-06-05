20596ed89e575e90b836ddd10be808dcd8303744
ferhat elmas <elmas.ferhat@gmail.com>
2015-06-05 18:19:46 +0200

    code eval string mask

---
+++ codeEval/easy/string_mask.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+
+with open(argv[1], 'r') as test_cases:
+    for test in test_cases:
+        print(''.join(
+            a if b == '0' else a.upper()
+            for a, b in zip(*test.rstrip().split())
+        ))
