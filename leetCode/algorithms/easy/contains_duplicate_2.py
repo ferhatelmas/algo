b33d7c00a230004fa39078f4104e7e43060adbe8
ferhat elmas <elmas.ferhat@gmail.com>
2015-06-07 13:51:45 +0200

    leetcode contains duplicate 2

---
+++ leetCode/algorithms/easy/contains_duplicate_2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        if k == 0:
+            return False
+        s = set(nums[:k+1])
+        if len(s) < len(nums[:k+1]):
+            return True
+        for i, e in enumerate(nums[k+1:]):
+            s.remove(nums[i])
+            if e in s:
+                return True
+            s.add(e)
+        return False
