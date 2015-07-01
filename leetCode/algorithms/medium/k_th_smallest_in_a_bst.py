8a518dbc359421bfb86cb1a2e6e225759094b20b
ferhat elmas <elmas.ferhat@gmail.com>
2015-07-02 01:54:15 +0200

    leetcode kth smallest in bst

---
+++ leetCode/algorithms/medium/k_th_smallest_in_a_bst.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kthSmallest(self, root, k):
+        self.k = k
+
+        def r(n):
+            if n:
+                c = r(n.left)
+                if not self.k:
+                    return c
+                self.k -= 1
+                if not self.k:
+                    return n.val
+                return r(n.right)
+        return r(root)
