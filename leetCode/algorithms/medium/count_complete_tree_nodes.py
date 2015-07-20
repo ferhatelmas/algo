6ec4b799cc41a554d1f2f129959266acd5f8336b
ferhat elmas <elmas.ferhat@gmail.com>
2015-07-20 23:48:24 +0200

    leetcode count complete tree nodes

---
+++ leetCode/algorithms/medium/count_complete_tree_nodes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countNodes(self, root):
+        if not root:
+            return 0
+        leftDepth = self.getDepth(root.left)
+        rightDepth = self.getDepth(root.right)
+        if leftDepth == rightDepth:
+            return 2**leftDepth + self.countNodes(root.right)
+        else:
+            return 2**rightDepth + self.countNodes(root.left)
+
+    def getDepth(self, root):
+        if not root:
+            return 0
+        return 1 + self.getDepth(root.left)
