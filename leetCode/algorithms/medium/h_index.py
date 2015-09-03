b125bd16ba96500167c43fcba5bbb54d95b5128f
ferhat elmas <elmas.ferhat@gmail.com>
2015-09-03 19:51:21 +0200

    leetcode h index

---
+++ leetCode/algorithms/medium/h_index.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hIndex(self, citations):
+        l = len(citations)
+        ls, r = [0] * (l+1), 0
+        for e in citations:
+            ls[min(e, l)] += 1
+        while l > -1:
+            r += ls[l]
+            if r >= l:
+                return l
+            l -= 1
