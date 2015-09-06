81435b056a845b924eae1c300647c5deed70a129
ferhat elmas <elmas.ferhat@gmail.com>
2015-09-06 19:51:21 +0200

    leetcode sliding window maximum

---
+++ leetCode/algorithms/hard/sliding_window_maximum.py
@@ -0,0 +1,6 @@
+# TODO: better algorithm
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        if not nums:
+            return []
+        return [max(nums[i:i+k]) for i in range(len(nums)-k+1)]
