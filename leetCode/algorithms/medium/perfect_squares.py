0841904e543707dbb2f77c609cc1fe268777e013
ferhat elmas <elmas.ferhat@gmail.com>
2015-09-10 00:07:34 +0200

    leetcode perfect squares

---
+++ leetCode/algorithms/medium/perfect_squares.py
@@ -0,0 +1,21 @@
+class Solution:
+    def primes(self, n):
+        d = 2
+        while d*d <= n:
+            c = 0
+            while (n % d) == 0:
+                c += 1
+                n //= d
+            yield (d, c)
+            d += 1
+        if n > 1:
+            yield (n, 1)
+
+    def numSquares(self, n):
+        while n % 4 == 0:
+            n /= 4
+        if n % 8 == 7:
+            return 4
+        if any(p % 4 == 3 and e % 2 == 1 for p, e in self.primes(n)):
+            return 3
+        return 1 if int(n**.5)**2 == n else 2
