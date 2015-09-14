cc4cbfb36b6a865870b0c8103f54ea1cc6108fc8
ferhat elmas <elmas.ferhat@gmail.com>
2015-09-14 23:54:20 +0200

    leetcode word ladder

---
+++ leetCode/algorithms/medium/word_ladder.py
@@ -0,0 +1,21 @@
+import string
+
+
+class Solution:
+    def ladderLength(self, start, end, d):
+        found, c = set([start]), 1
+        d.add(end)
+        while end not in found:
+            new_founds = set()
+            for s in found:
+                for i, e in enumerate(s):
+                    for ch in string.ascii_lowercase:
+                        w = s[:i] + ch + s[i+1:]
+                        if w in d:
+                            new_founds.add(w)
+                            d.remove(w)
+            c += 1
+            if not new_founds:
+                return 0
+            found = new_founds
+        return c
