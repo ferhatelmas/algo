9031ee4a164e64e609277427f9ba2cf84fb872f7
ferhat elmas <elmas.ferhat@gmail.com>
2015-09-18 19:32:10 +0200

    leetcode move zeroes

---
+++ leetCode/algorithms/easy/move_zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums):
+        for i, e in enumerate(nums[1:], start=1):
+            if e == 0:
+                continue
+            j = i
+            while j > 0 and nums[j-1] == 0:
+                j -= 1
+            nums[i], nums[j] = 0, e
