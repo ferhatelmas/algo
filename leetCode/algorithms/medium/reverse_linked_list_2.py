2a29e8666e017203896325c21be4c441613b6d9d
ferhat elmas <elmas.ferhat@gmail.com>
2015-10-06 02:32:17 +0200

    leetcode reverse linked list 2

---
+++ leetCode/algorithms/medium/reverse_linked_list_2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseBetween(self, head, m, n):
+        t = ListNode(0)
+        t.next = head
+        p, s = t, head
+        for _ in range(m-1):
+            p, s = p.next, s.next
+        for _ in range(n-m):
+            c = s.next
+            s.next, c.next = c.next, p.next
+            p.next = c
+        return t.next
