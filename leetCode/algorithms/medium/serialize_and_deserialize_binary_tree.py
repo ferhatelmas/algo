066174630b5cedd5d11fc19ee33f7c8b188733e4
ferhat elmas <elmas.ferhat@gmail.com>
2015-10-27 00:33:43 +0100

    leetcode serialize and deserialize binary tree

---
+++ leetCode/algorithms/medium/serialize_and_deserialize_binary_tree.py
@@ -0,0 +1,26 @@
+class Codec:
+    def serialize(self, root):
+        s = []
+
+        def r(n):
+            if n:
+                s.append(str(n.val))
+                r(n.left)
+                r(n.right)
+            else:
+                s.append("~")
+        r(root)
+        return " ".join(s)
+
+    def deserialize(self, data):
+        s = iter(data.split())
+
+        def p():
+            v = next(s)
+            if v == "~":
+                return None
+            n = TreeNode(int(v))
+            n.left = p()
+            n.right = p()
+            return n
+        return p()
