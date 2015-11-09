b819eb892c1c273f472ec3803ce012d7315e6e5b
ferhat elmas <elmas.ferhat@gmail.com>
2015-11-09 19:32:10 +0100

    leetcode range sum query

---
+++ leetCode/algorithms/easy/range_sum_query.py
@@ -0,0 +1,11 @@
+class NumArray(object):
+    def __init__(self, nums):
+        self.s = []
+        for i, e in enumerate(nums):
+            self.s.append(e + (self.s[i-1] if i > 0 else 0))
+
+    def sumRange(self, i, j):
+        n = self.s[j]
+        if i > 0:
+            n -= self.s[i-1]
+        return n
