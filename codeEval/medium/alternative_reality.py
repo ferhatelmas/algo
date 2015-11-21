5d952a665a715fde90ff48f995871e6c555e047e
ferhat elmas <elmas.ferhat@gmail.com>
2015-11-21 09:21:11 +0100

    code eval alternative reality

---
+++ codeEval/medium/alternative_reality.py
@@ -0,0 +1,22 @@
+import sys
+
+
+cache = {}
+coins = (1, 5, 10, 25, 50)
+
+
+def solve(t, k):
+    if t < 0 or k <= 0:
+        return 0
+    if t == 0:
+        return 1
+    i = 5 * t + k
+    if i in cache:
+        return cache[i]
+    cache[i] = solve(t, k-1) + solve(t - coins[k-1], k)
+    return cache[i]
+
+
+with open(sys.argv[1], 'r') as test_cases:
+    for test in test_cases:
+        print(solve(int(test), 5))
