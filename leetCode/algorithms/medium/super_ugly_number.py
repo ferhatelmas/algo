8f3d5eacf86c11b2c74ea108e4d78c7abc02dad4
ferhat elmas <elmas.ferhat@gmail.com>
2016-01-24 23:45:37 +0100

    leetcode super ugly number

---
+++ leetCode/algorithms/medium/super_ugly_number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nthSuperUglyNumber(self, n, primes):
+        u, l = [1], 1
+        idx = [0] * len(primes)
+        while l < n:
+            nu = min(u[idx[i]] * p for i, p in enumerate(primes))
+            for i, p in enumerate(primes):
+                if nu == u[idx[i]] * p:
+                    idx[i] += 1
+            u.append(nu)
+            l += 1
+        return u[n-1]
