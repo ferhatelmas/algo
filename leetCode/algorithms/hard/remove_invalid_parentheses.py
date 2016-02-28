1cad0d7d43c7209bbbebc01c5b1d7e3f9d235911
ferhat elmas <elmas.ferhat@gmail.com>
2016-02-28 23:38:29 +0100

    leetcode remove invalid parentheses

---
+++ leetCode/algorithms/hard/remove_invalid_parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeInvalidParentheses(self, s):
+        r = []
+        self.remove(s, r, 0, 0, "()")
+        return r
+
+    def remove(self, s, r, li, lj, c):
+        cnt = 0
+        for i, e in enumerate(s[li:], start=li):
+            cnt += (1, -1, 0)[c.find(e)]
+            if cnt < 0:
+                for j, f in enumerate(s[lj:i+1], start=lj):
+                    if f == c[1] and (j == lj or s[j-1] != c[1]):
+                        self.remove(s[:j] + s[j+1:], r, i, j, c)
+                return
+        if c[0] == "(":
+            self.remove(s[::-1], r, 0, 0, ")(")
+        else:
+            r.append(s[::-1])
