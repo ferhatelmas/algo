e103ae7e4471a0c3d2aff8cbfbbd8ba096c07fd5
ferhat elmas <elmas.ferhat@gmail.com>
2016-03-03 22:06:52 +0100

    leetcode combination sum 2

---
+++ leetCode/algorithms/medium/combination_sum_2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        return self.combination(sorted(candidates), target, 0, [], [])
+
+    def combination(self, cands, t, n, comb, res):
+        for i, e in enumerate(cands[n:], start=n):
+            if i > n and e == cands[i-1]:
+                continue
+            if e == t:
+                res.append(comb + [e])
+            elif e < t:
+                self.combination(cands, t - cands[i], i + 1, comb + [e], res)
+            else:
+                break
+        return res
