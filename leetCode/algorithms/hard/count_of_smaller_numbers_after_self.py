81914b82f406399049c487ec073102419544274a
ferhat elmas <elmas.ferhat@gmail.com>
2016-03-10 23:11:49 +0100

    leetcode count smaller after self

---
+++ leetCode/algorithms/hard/count_of_smaller_numbers_after_self.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countSmaller(self, nums):
+        l = len(nums)
+        res, index = [0] * l, range(l-1, -1, -1)
+        if l > 0:
+            m = abs(min(nums))
+            nums = [e + m for e in nums]
+
+        def f(ind, m):
+            if m == 0 or len(ind) < 2:
+                return
+
+            low, high, ls = [], [], 0
+            for i, e in enumerate(ind):
+                if (nums[e] & m) == m:
+                    res[e] += ls
+                    high.append(e)
+                else:
+                    ls += 1
+                    low.append(e)
+            f(low, m >> 1)
+            f(high, m >> 1)
+        f(index, 1 << 31)
+        return res
