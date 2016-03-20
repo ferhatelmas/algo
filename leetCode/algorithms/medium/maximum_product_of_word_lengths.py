d32aa35f33a491357c6eb602fae9ef421791f16c
ferhat elmas <elmas.ferhat@gmail.com>
2016-03-20 22:59:40 +0100

    leetcode max product of word lengths

---
+++ leetCode/algorithms/medium/maximum_product_of_word_lengths.py
@@ -0,0 +1,16 @@
+import operator
+
+
+class Solution:
+    def maxProduct(self, words):
+        m, ws = 0, sorted(((set(w), len(w)) for w in words),
+                          key=operator.itemgetter(1), reverse=True)
+        for i, a in enumerate(ws):
+            for b in ws[i+1:]:
+                if not (a[0] & b[0]):
+                    n = a[1] * b[1]
+                    if n > m:
+                        m = n
+                    else:
+                        break
+        return m
