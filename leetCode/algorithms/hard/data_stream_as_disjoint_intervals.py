b705505086521690f951da8804b36fdc54a505f8
ferhat elmas <elmas.ferhat@gmail.com>
2016-06-05 23:33:13 +0200

    leetcode data stream as disjoint intervals

---
+++ leetCode/algorithms/hard/data_stream_as_disjoint_intervals.py
@@ -0,0 +1,31 @@
+import heapq
+
+
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class SummaryRanges(object):
+
+    def __init__(self):
+        self.intervals = []
+
+    def addNum(self, val):
+        heapq.heappush(self.intervals, (val, Interval(val, val)))
+
+    def getIntervals(self):
+        intervals = []
+        while self.intervals:
+            i = heapq.heappop(self.intervals)
+            if not intervals:
+                intervals.append(i)
+            else:
+                _, prev = intervals[-1]
+                if prev.end + 1 >= i[1].start:
+                    prev.end = max(prev.end, i[1].end)
+                else:
+                    intervals.append(i)
+        self.intervals = intervals
+        return [iv for v, iv in intervals]
