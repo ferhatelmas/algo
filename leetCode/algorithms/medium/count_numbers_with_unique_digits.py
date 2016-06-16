8c4f9c371fa60cfdbd6253800d866e6db4609663
ferhat elmas <elmas.ferhat@gmail.com>
2016-06-17 00:40:51 +0200

    leetcode count numbers with unique digits

---
+++ leetCode/algorithms/medium/count_numbers_with_unique_digits.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        def base(s, n):
+            b = s[1] * (11 - n)
+            return s[0] + b, b
+        return reduce(base, range(2, min(n, 10) + 1), (min(10, 10**n), 9))[0]
