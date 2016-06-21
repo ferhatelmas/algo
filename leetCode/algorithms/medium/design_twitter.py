81a9d937f4e8edc404fadad8ad4aa150cd5da529
ferhat elmas <elmas.ferhat@gmail.com>
2016-06-21 02:12:27 +0200

    leetcode design twitter

---
+++ leetCode/algorithms/medium/design_twitter.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+
+class Twitter(object):
+    def __init__(self):
+        self.tweets = defaultdict(list)
+        self.followers = defaultdict(set)
+        self.t = 0
+
+    def postTweet(self, userId, tweetId):
+        self.tweets[userId].append((self.t, tweetId))
+        self.t += 1
+
+    def getNewsFeed(self, userId):
+        tweets = sorted([
+            f
+            for i in set([userId]) | self.followers[userId]
+            for f in self.tweets[i]
+        ], reverse=True)[:10]
+        return [t[1] for t in tweets]
+
+    def follow(self, followerId, followeeId):
+        self.followers[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        s = self.followers[followerId]
+        if followeeId in s:
+            s.remove(followeeId)
