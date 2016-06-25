66aa56aab489ac2c1a8c1e16174a898b8a381ad0
ferhat elmas <elmas.ferhat@gmail.com>
2016-06-25 22:02:01 +0200

    leetcode water and jug problem

---
+++ leetCode/algorithms/medium/water_and_jug_problem.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def canMeasureWater(self, x, y, z):
+        def gcd(a, b):
+            return a if b == 0 else gcd(b, a % b)
+        return z == 0 or z <= x + y and z % gcd(x, y) == 0
