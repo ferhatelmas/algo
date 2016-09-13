2ebb188df00ebf1741c0a6f96a1fd5aa43d847a9
ferhat elmas <elmas.ferhat@gmail.com>
2016-09-14 01:42:32 +0200

    leetcode integer replacement

---
+++ leetCode/algorithms/easy/integer_replacement.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def integerReplacement(self, n):
+        def s(x, c):
+            if x == 1:
+                return c
+            if x % 2 == 0:
+                return s(x/2, c+1)
+            return min(s(x-1, c+1), s(x+1, c+1))
+        return s(n, 0)
