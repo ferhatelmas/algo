2f1ebd1b1373ba420a80490add157d82ea75e51b
ferhat elmas <elmas.ferhat@gmail.com>
2016-11-21 00:01:26 +0100

    leetcode fizz buzz

---
+++ leetCode/algorithms/easy/fizz_buzz.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        def c(x):
+            if x % 15 == 0:
+                return "FizzBuzz"
+            elif x % 3 == 0:
+                return "Fizz"
+            elif x % 5 == 0:
+                return "Buzz"
+            return str(x)
+        return [c(x) for x in range(1, n+1)]
