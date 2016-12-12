001644c99983c56f2a0805b560fc4b6f8c5eff6b
ferhat elmas <elmas.ferhat@gmail.com>
2016-12-12 23:36:12 +0100

    leetcode heaters

---
+++ leetCode/algorithms/easy/heaters.py
@@ -0,0 +1,17 @@
+import bisect
+
+
+class Solution(object):
+    def findRadius(self, houses, heaters):
+        heaters.sort()
+        radius, l = 0, len(heaters)
+        for h in houses:
+            current = 10**9
+            left = bisect.bisect_right(heaters, h)
+            if left:
+                current = min(current, h-heaters[left-1])
+            right = bisect.bisect_left(heaters, h)
+            if right != l:
+                current = min(current, heaters[right]-h)
+            radius = max(radius, current)
+        return radius
