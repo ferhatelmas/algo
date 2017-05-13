f52a27d65cba24ca642476e9300a19b3ecceb78b
ferhat elmas <elmas.ferhat@gmail.com>
2017-05-13 22:40:12 +0200

    leetcode student attendance record 1

---
+++ leetCode/algorithms/easy/student_attendance_record_1.py
@@ -0,0 +1,8 @@
+from itertools import groupby
+
+
+class Solution(object):
+    def checkRecord(self, s):
+        absent = s.count('A') > 1
+        late = any(len(list(g)) > 2 for k, g in groupby(s) if k == 'L')
+        return not absent and not late
