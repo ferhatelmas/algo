be8cf4bb8593f70cc954ef510a12e2349164cc69
ferhat elmas <elmas.ferhat@gmail.com>
2017-05-16 23:36:12 +0200

    leetcode keyboard row

---
+++ leetCode/algorithms/easy/keyboard_row.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def findWords(self, words):
+        rows = [set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')]
+        return [w for w in words
+                if sum(set(w.lower()) <= row for row in rows) == 1]
