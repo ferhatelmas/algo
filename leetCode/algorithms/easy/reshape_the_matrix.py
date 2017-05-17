c3b4a2e24e56fe734b9b5d4afc20fa2c774847bf
ferhat elmas <elmas.ferhat@gmail.com>
2017-05-17 03:31:40 +0200

    leetcode reshape the matrix

---
+++ leetCode/algorithms/easy/reshape_the_matrix.py
@@ -0,0 +1,10 @@
+import itertools
+
+
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        l = len(nums) * len(nums[0])
+        if l != r * c:
+            return nums
+        it = itertools.chain(*nums)
+        return [list(itertools.islice(it, 0, c)) for i in xrange(0, l, c)]
