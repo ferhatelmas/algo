ea158d6290faefc6d26623bf197e8a1db8c43e9b
ferhat elmas <elmas.ferhat@gmail.com>
2019-04-03 01:06:47 +0200

    leetcode unique email addresses

---
+++ leetCode/algorithms/easy/unique_email_addresses.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        return len(set(self.unique(e) for e in emails))
+
+    def unique(self, email: str) -> str:
+        local, domain = email.split("@")
+        real, *ignored = local.split("+", 1)
+        return real.replace(".", "") + "@" + domain
