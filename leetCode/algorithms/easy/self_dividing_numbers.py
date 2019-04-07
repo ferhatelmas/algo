cac71352bb34409d143bef6a7d490003256161bb
ferhat elmas <elmas.ferhat@gmail.com>
2019-04-07 18:23:19 +0200

    leetcode self dividing numbers

---
+++ leetCode/algorithms/easy/self_dividing_numbers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def selfDividing(self, n: int) -> bool:
+        s = str(n)
+        return '0' not in s and all(n % int(e) == 0 for e in s)
+
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        return [n for n in range(left, right+1) if self.selfDividing(n)]
