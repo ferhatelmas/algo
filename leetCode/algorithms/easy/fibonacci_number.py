c52b4be9b9aa3af51a436b7fae1faa040a9a9ff2
ferhat elmas <elmas.ferhat@gmail.com>
2019-04-14 14:08:58 +0200

    leetcode fibonacci number

---
+++ leetCode/algorithms/easy/fibonacci_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        if N < 2:
+            return N
+        a = [0, 1]
+        for i in range(2, N+1):
+            a.append(a[i-2] + a[i-1])
+        return a[N]
