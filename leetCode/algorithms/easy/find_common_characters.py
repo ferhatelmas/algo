559cd5309c3aff539bf241e9aec614dac88d588d
ferhat elmas <elmas.ferhat@gmail.com>
2019-04-19 01:42:46 +0200

    leetcode find common characters

---
+++ leetCode/algorithms/easy/find_common_characters.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        c = Counter(A[0])
+        for e in A[1:]:
+            c = c & Counter(e)
+        return list(c.elements())
