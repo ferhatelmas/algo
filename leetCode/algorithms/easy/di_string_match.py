41d40cbee7f5acb27e10cecb8bfead8f412ffce6
ferhat elmas <elmas.ferhat@gmail.com>
2019-07-28 17:28:30 +0200

    leetcode di string match

---
+++ leetCode/algorithms/easy/di_string_match.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def diStringMatch(self, S: str) -> List[int]:
+        res, a, b = [0], 0, 0
+        for e in S:
+            if e == 'I':
+                a += 1
+                res.append(a)
+            else:
+                b -= 1
+                res.append(b)
+        return [e - b for e in res]
