b3dd1f8aa2904d74fe2bbae8fccd6f8df0f75a17
ferhat elmas <elmas.ferhat@gmail.com>
2019-07-29 00:51:27 +0200

    leetcode peak index in a mountain array

---
+++ leetCode/algorithms/easy/peak_index_in_a_mountain_array.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        for i, e in enumerate(A):
+            if e > A[i+1]:
+                return i
+        return -1
