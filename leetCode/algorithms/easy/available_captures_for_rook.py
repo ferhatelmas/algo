aa1837fe54c9f56ed98a414c4987af3d78da5d52
ferhat elmas <elmas.ferhat@gmail.com>
2019-07-29 23:25:55 +0200

    leetcode available captures for rook

---
+++ leetCode/algorithms/easy/available_captures_for_rook.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def numRookCaptures(self, board: List[List[str]]) -> int:
+        cnt = 0
+        for i, r in enumerate(board):
+            for j, c in enumerate(r):
+                if c != 'R':
+                    continue
+                k = j + 1
+                while k < 8 and r[k] == '.':
+                    k += 1
+                cnt += (k < 8 and r[k] == 'p')
+
+                k = j - 1
+                while k > -1 and r[k] == '.':
+                    k -= 1
+                cnt += (k > -1 and r[k] == 'p')
+
+                k = i + 1
+                while k < 8 and board[k][j] == '.':
+                    k += 1
+                cnt += (k < 8 and board[k][j] == 'p')
+
+                k = i - 1
+                while k > -1 and board[k][j] == '.':
+                    k -= 1
+                cnt += (k > -1 and board[k][j] == 'p')
+        return cnt
