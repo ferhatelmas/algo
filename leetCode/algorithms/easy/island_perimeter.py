6139bcdcbe14dca551bde1c12c4b8c9a87238f28
ferhat elmas <elmas.ferhat@gmail.com>
2019-08-03 00:43:01 +0200

    leetcode island perimeter

---
+++ leetCode/algorithms/easy/island_perimeter.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        cnt = 0
+        for i, row in enumerate(grid):
+            for j, col in enumerate(row):
+                if col == 0:
+                    continue
+                if i == 0 or grid[i-1][j] == 0:
+                  cnt += 1
+                if j == 0 or row[j-1] == 0:
+                  cnt += 1
+                if i == len(grid) - 1 or grid[i+1][j] == 0:
+                  cnt += 1
+                if j == len(row) - 1 or row[j+1] == 0:
+                  cnt += 1
+        return cnt
