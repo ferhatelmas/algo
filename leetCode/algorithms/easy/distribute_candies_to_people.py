c5b7e8e05d8c16f8f30878f89142a63d76208992
ferhat elmas <elmas.ferhat@gmail.com>
2019-08-02 23:29:41 +0200

    leetcode distribute candies to people

---
+++ leetCode/algorithms/easy/distribute_candies_to_people.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        res, inc, index = [0] * num_people, 1, 0
+        while candies > 0:
+            res[index] += min(inc, candies)
+            index, inc, candies = (index+1) % num_people, inc + 1, candies - inc
+        return res
