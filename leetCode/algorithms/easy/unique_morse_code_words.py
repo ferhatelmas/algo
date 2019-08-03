e183b5f7e3e612516e0505ceb4c3fee500196df9
ferhat elmas <elmas.ferhat@gmail.com>
2019-08-03 02:36:31 +0200

    leetcode unique morse code words

---
+++ leetCode/algorithms/easy/unique_morse_code_words.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = [
+            ".-",
+            "-...",
+            "-.-.",
+            "-..",
+            ".",
+            "..-.",
+            "--.",
+            "....",
+            "..",
+            ".---",
+            "-.-",
+            ".-..",
+            "--",
+            "-.",
+            "---",
+            ".--.",
+            "--.-",
+            ".-.",
+            "...",
+            "-",
+            "..-",
+            "...-",
+            ".--",
+            "-..-",
+            "-.--",
+            "--..",
+        ]
+        return len(set("".join(morse[ord(e) - ord("a")] for e in w) for w in words))
