32810248215a44d98adba8db8537bc80c3e71064
ferhat elmas <elmas.ferhat@gmail.com>
2019-08-23 22:41:49 +0200

    leetcode print in order

---
+++ leetCode/algorithms/easy/print_in_order.py
@@ -0,0 +1,22 @@
+from threading import Lock
+
+
+class Foo:
+    def __init__(self):
+        self.first_lock = Lock()
+        self.second_lock = Lock()
+        self.first_lock.acquire()
+        self.second_lock.acquire()
+
+    def first(self, printFirst: "Callable[[], None]") -> None:
+        printFirst()
+        self.first_lock.release()
+
+    def second(self, printSecond: "Callable[[], None]") -> None:
+        self.first_lock.acquire()
+        printSecond()
+        self.second_lock.release()
+
+    def third(self, printThird: "Callable[[], None]") -> None:
+        self.second_lock.acquire()
+        printThird()
