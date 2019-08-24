d26845e4face5b478938ffc4a479ec287991b3ea
ferhat elmas <elmas.ferhat@gmail.com>
2019-08-24 23:30:51 +0200

    leetcode print foobar alternately

---
+++ leetCode/algorithms/medium/print_foobar_alternately.py
@@ -0,0 +1,21 @@
+from threading import Lock
+
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.lock1 = Lock()
+        self.lock2 = Lock()
+        self.lock2.acquire()
+
+    def foo(self, printFoo: "Callable[[], None]") -> None:
+        for _ in range(self.n):
+            self.lock1.acquire()
+            printFoo()
+            self.lock2.release()
+
+    def bar(self, printBar: "Callable[[], None]") -> None:
+        for _ in range(self.n):
+            self.lock2.acquire()
+            printBar()
+            self.lock1.release()
