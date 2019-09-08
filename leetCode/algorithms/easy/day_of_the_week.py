fec06d8d64fcd817c4ea52452d53898d750dc6ff
ferhat elmas <elmas.ferhat@gmail.com>
2019-09-08 14:51:56 +0200

    leetcode day of the week w/ isoweekday

--- leetCode/algorithms/easy/day_of_the_week.py
+++ leetCode/algorithms/easy/day_of_the_week.py
@@ -4,12 +4,13 @@ from datetime import date
 class Solution:
     def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
         days = [
-            "Sunday",
+            "",
             "Monday",
             "Tuesday",
             "Wednesday",
             "Thursday",
             "Friday",
             "Saturday",
+            "Sunday",
         ]
-        return days[(date(year, month, day).weekday() + 1) % len(days)]
+        return days[date(year, month, day).isoweekday()]
