462b631d11d993049efb5cbe192a35482da5d45e
ferhat elmas <elmas.ferhat@gmail.com>
2019-09-09 22:05:16 +0200

    leetcode drop repeated nth tribonacci number

--- leetCode/algorithms/easy/n_th_tribonacci_number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        fib = [0, 1, 1]
-        for i in range(3, n + 1):
-            fib.append(fib[i - 1] + fib[i - 2] + fib[i - 3])
-        return fib[n]
