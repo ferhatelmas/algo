e07dee5c24b21d6e0c1b3e8d087dd6fdb044a97e
ferhat elmas <elmas.ferhat@gmail.com>
2019-09-29 20:33:50 +0200

    chore: rm extra line

--- leetCode/algorithms/easy/minimum_absolute_difference.py
+++ leetCode/algorithms/easy/minimum_absolute_difference.py
@@ -7,4 +7,3 @@ class Solution:
         l = len(arr)
         m = min(arr[i] - arr[i - 1] for i in range(1, l))
         return [[arr[i - 1], arr[i]] for i in range(1, l) if arr[i] - arr[i - 1] == m]
-
