72d48d6da92c4832b918ab9f6a465b57a3e1910f
ferhat elmas <elmas.ferhat@gmail.com>
2020-03-17 22:12:56 +0100

    leetcode number of days between two dates

---
+++ leetCode/algorithms/easy/number_of_days_between_two_dates.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        d1 = datetime.strptime(date1, "%Y-%m-%d")
+        d2 = datetime.strptime(date2, "%Y-%m-%d")
+        return abs((d1 - d2).days)
