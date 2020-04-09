6edef488fab43b796f3de543e2452b0474c27b87
ferhat elmas <elmas.ferhat@gmail.com>
2020-04-09 23:57:33 +0200

    leetcode reducing dishes

---
+++ leetCode/algorithms/hard/reducing_dishes.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        ss, l, m = sorted(satisfaction), len(satisfaction), 0
+        for i in range(0, l):
+            m = max(m, sum(t * s for t, s in enumerate(ss[i:], start=1)))
+        return m
