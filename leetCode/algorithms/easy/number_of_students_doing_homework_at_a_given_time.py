5681448733e459d5c091c473e00b1721958781ac
ferhat elmas <elmas.ferhat@gmail.com>
2020-05-22 20:46:49 +0200

    leetcode number of students doing hw

---
+++ leetCode/algorithms/easy/number_of_students_doing_homework_at_a_given_time.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def busyStudent(
+        self, startTime: List[int], endTime: List[int], queryTime: int
+    ) -> int:
+        return sum(a <= queryTime <= b for a, b in zip(startTime, endTime))
