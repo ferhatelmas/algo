feaf0a0dafb0e00ed65f0e42a3b3d8edff95097f
ferhat elmas <elmas.ferhat@gmail.com>
2020-08-26 03:20:48 +0200

    leetcode three consecutive odds

---
+++ leetCode/algorithms/easy/three_consecutive_odds.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(0, len(arr) - 2):
+            if arr[i] % 2 == 1 and arr[i + 1] % 2 == 1 and arr[i + 2] % 2 == 1:
+                return True
+        return False
