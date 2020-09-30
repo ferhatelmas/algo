397859a53429e7a585e5f6964ad24146c6261326
Gaurav Kumar <59891376+gauravsingh58@users.noreply.github.com>
2020-10-01 01:07:53 +0530

    Update climbing_stairs.py

--- leetCode/algorithms/easy/climbing_stairs.py
+++ leetCode/algorithms/easy/climbing_stairs.py
@@ -10,3 +10,17 @@ class Solution:
         if n > 1:
             self.cache[n] += self.climbStairs(n - 2)
         return self.cache[n]
+    //the cpp solution
+    class Solution {
+public:
+    int climbStairs(int n) {
+      long int dp[n+1];
+        dp[0]=1,dp[1]=2;
+        if(n<=2) return n;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
+    }
+};
