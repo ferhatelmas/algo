1b2bb621c79ee854211eb5491f0429ccee210253
Gaurav Kumar <59891376+gauravsingh58@users.noreply.github.com>
2020-10-01 01:01:14 +0530

    Update island_perimeter.py

--- leetCode/algorithms/easy/island_perimeter.py
+++ leetCode/algorithms/easy/island_perimeter.py
@@ -17,3 +17,37 @@ class Solution:
                 if j == len(row) - 1 or row[j + 1] == 0:
                     cnt += 1
         return cnt
+    /*class Solution {
+public:
+    int cnt=0;
+    void dfs(vector<vector<int>>& grid,int i,int j)
+    {
+        if(i<0||i>=grid.size()||j<0||j>=grid[i].size()||grid[i][j]==0)
+        {
+            cnt++;
+            return;
+        }
+        if(grid[i][j]==-1) return;
+        grid[i][j]=-1;
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
+    int islandPerimeter(vector<vector<int>>& grid) {
+        cnt=0;
+        //int n=grid.size(),m=grid[0].size();
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[i].size();j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    dfs(grid,i,j);
+                   break;
+                }
+            }
+        }
+        return cnt;
+    }
+};*/
