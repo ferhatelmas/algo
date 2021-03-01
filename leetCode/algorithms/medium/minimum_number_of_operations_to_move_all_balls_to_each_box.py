4aca95080b501f417846234799ab68bd4c572eed
ferhat elmas <elmas.ferhat@gmail.com>
2021-03-02 00:18:20 +0100

    leetcode minimum number of operations to move all balls to each box

---
+++ leetCode/algorithms/medium/minimum_number_of_operations_to_move_all_balls_to_each_box.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        return [sum(abs(i-j) for i, a in enumerate(boxes) for j, b in enumerate(boxes) if b == '1')]
