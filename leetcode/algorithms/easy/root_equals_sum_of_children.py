abebe105f01682b7888b4730c20f6d612717e13b
ferhat elmas <elmas.ferhat@gmail.com>
2022-04-12 23:41:09 +0200

    leetcode root equals sum of children

---
+++ leetcode/algorithms/easy/root_equals_sum_of_children.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == (root.left.val + root.right.val)
