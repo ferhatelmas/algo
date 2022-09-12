8bcfda5642bd4a37e2c704d2d27537ed44062d54
ferhat elmas <elmas.ferhat@gmail.com>
2022-09-12 23:15:55 +0200

    leetcode maximum product of three numbers

---
+++ leetcode/algorithms/easy/maximum_product_of_three_numbers.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
