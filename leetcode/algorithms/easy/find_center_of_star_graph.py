55503c79a7a4dffb15edbb2f9d3246e59e8d4398
ferhat elmas <elmas.ferhat@gmail.com>
2023-10-26 11:56:11 +0200

    leetcode center of star graph

---
+++ leetcode/algorithms/easy/find_center_of_star_graph.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        for i in edges[0]:
+            if i in edges[1]:
+                return i
